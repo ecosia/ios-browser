mozilla-mobile/firefox-ios

Bug 1129161 - Allow disconnecting the Firefox Account.
The message is from Desktop.  UX talked about a button at the bottom of
the settings panel, but as bnicholson is aware this is a pain with a
UITableViewController.  This will do for now.

Client/Frontend/Settings/SettingsTableViewController.swift
@@ -7,7 +7,11 @@ import UIKit
 
 class SettingsTableViewController: UITableViewController {
     let SECTION_ACCOUNT = 0
+    let ITEM_ACCOUNT_STATUS = 0
+    let ITEM_ACCOUNT_DISCONNECT = 1
+
     let SECTION_SEARCH = 1
+
     let NUMBER_OF_SECTIONS = 2
 
     var profile: Profile!
@@ -26,9 +30,16 @@ class SettingsTableViewController: UITableViewController {
         var cell: UITableViewCell
 
         if indexPath.section == SECTION_ACCOUNT {
-            cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: nil)
-            cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
-            updateCell(cell, toReflectAccount: profile.getAccount())
+            if indexPath.item == ITEM_ACCOUNT_STATUS {
+                cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: nil)
+                cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+                updateCell(cell, toReflectAccount: profile.getAccount())
+            } else if indexPath.item == ITEM_ACCOUNT_DISCONNECT {
+                cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+                cell.textLabel?.text = NSLocalizedString("Disconnect", comment: "Settings")
+            } else {
+                cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+            }
         } else if indexPath.section == SECTION_SEARCH {
             cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
             cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
@@ -49,7 +60,13 @@ class SettingsTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == SECTION_ACCOUNT {
-            return 1
+            if profile.getAccount() == nil {
+                // Just "Sign in".
+                return 1
+            } else {
+                // Account state, and "Disconnect."
+                return 2
+            }
         } else if section == SECTION_SEARCH {
             return 1
         } else {
@@ -69,26 +86,35 @@ class SettingsTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
         if indexPath.section == SECTION_ACCOUNT {
-            let viewController = FxAContentViewController()
-            viewController.delegate = self
-            if let account = profile.getAccount() {
-                switch account.getActionNeeded() {
-                case .None, .NeedsVerification:
-                    let cs = NSURLComponents(URL: profile.accountConfiguration.settingsURL, resolvingAgainstBaseURL: false)
-                    cs?.queryItems?.append(NSURLQueryItem(name: "email", value: account.email))
-                    viewController.url = cs?.URL
-                case .NeedsPassword:
-                    let cs = NSURLComponents(URL: profile.accountConfiguration.forceAuthURL, resolvingAgainstBaseURL: false)
-                    cs?.queryItems?.append(NSURLQueryItem(name: "email", value: account.email))
-                    viewController.url = cs?.URL
-                case .NeedsUpgrade:
-                    // In future, we'll want to link to an upgrade page.
-                    break
+            switch indexPath.item {
+            case ITEM_ACCOUNT_STATUS:
+                let viewController = FxAContentViewController()
+                viewController.delegate = self
+                if let account = profile.getAccount() {
+                    switch account.getActionNeeded() {
+                    case .None, .NeedsVerification:
+                        let cs = NSURLComponents(URL: profile.accountConfiguration.settingsURL, resolvingAgainstBaseURL: false)
+                        cs?.queryItems?.append(NSURLQueryItem(name: "email", value: account.email))
+                        viewController.url = cs?.URL
+                    case .NeedsPassword:
+                        let cs = NSURLComponents(URL: profile.accountConfiguration.forceAuthURL, resolvingAgainstBaseURL: false)
+                        cs?.queryItems?.append(NSURLQueryItem(name: "email", value: account.email))
+                        viewController.url = cs?.URL
+                    case .NeedsUpgrade:
+                        // In future, we'll want to link to an upgrade page.
+                        break
+                    }
+                } else {
+                    viewController.url = profile.accountConfiguration.signInURL
                 }
-            } else {
-                viewController.url = profile.accountConfiguration.signInURL
+                navigationController?.pushViewController(viewController, animated: true)
+
+            case ITEM_ACCOUNT_DISCONNECT:
+                maybeDisconnectAccount()
+
+            default:
+                break
             }
-            navigationController?.pushViewController(viewController, animated: true)
         } else if indexPath.section == SECTION_SEARCH {
             let viewController = SearchSettingsTableViewController()
             viewController.model = profile.searchEngines
@@ -119,6 +145,23 @@ class SettingsTableViewController: UITableViewController {
         } else {
             cell.textLabel?.text = NSLocalizedString("Sign in", comment: "Settings")
         }
+    }
+
+    func maybeDisconnectAccount() {
+        let alertController = UIAlertController(
+            title: NSLocalizedString("Disconnect Firefox Account?", comment: "Settings"),
+            message: NSLocalizedString("Firefox will stop syncing with your account, but won’t delete any of your browsing data on this device.", comment: "Settings"),
+            preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(
+            UIAlertAction(title: NSLocalizedString("Cancel", comment: "Settings"), style: .Cancel) { (action) in
+                // Do nothing.
+            })
+        alertController.addAction(
+            UIAlertAction(title: NSLocalizedString("Disconnect", comment: "Settings"), style: .Destructive) { (action) in
+                self.profile.setAccount(nil)
+                self.tableView.reloadData()
+            })
+        presentViewController(alertController, animated: true, completion: nil)
     }
 }
 

ecosia/ios-browser

Client/Frontend/Settings/SettingsTableViewController.swift
@@ -7,6 +7,8 @@ import UIKit
 
 class SettingsTableViewController: UITableViewController {
     let SectionAccount = 0
+    let ItemAccountStatus = 0
+    let ItemAccountDisconnect = 1
     let SectionSearch = 1
     let NumberOfSections = 2
 
@@ -26,10 +28,17 @@ class SettingsTableViewController: UITableViewController {
         var cell: UITableViewCell
 
         if indexPath.section == SectionAccount {
-            cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: nil)
-            cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
-            updateCell(cell, toReflectAccount: profile.getAccount())
-        } else if indexPath.section == SECTION_SEARCH {
+            if indexPath.item == ItemAccountStatus {
+                cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: nil)
+                cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+                updateCell(cell, toReflectAccount: profile.getAccount())
+            } else if indexPath.item == ItemAccountDisconnect {
+                cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+                cell.textLabel?.text = NSLocalizedString("Disconnect", comment: "Settings")
+            } else {
+                cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+            }
+        } else if indexPath.section == SectionSearch {
             cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
             cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
             cell.textLabel?.text = NSLocalizedString("Search", comment: "Settings")
@@ -49,7 +58,13 @@ class SettingsTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if section == SectionAccount {
-            return 1
+            if profile.getAccount() == nil {
+                // Just "Sign in".
+                return 1
+            } else {
+                // Account state, and "Disconnect."
+                return 2
+            }
         } else if section == SectionSearch {
             return 1
         } else {
@@ -69,26 +84,35 @@ class SettingsTableViewController: UITableViewController {
 
     override func tableView(tableView: UITableView, willSelectRowAtIndexPath indexPath: NSIndexPath) -> NSIndexPath? {
         if indexPath.section == SectionAccount {
-            let viewController = FxAContentViewController()
-            viewController.delegate = self
-            if let account = profile.getAccount() {
-                switch account.getActionNeeded() {
-                case .None, .NeedsVerification:
-                    let components = NSURLComponents(URL: profile.accountConfiguration.settingsURL, resolvingAgainstBaseURL: false)
-                    components?.queryItems?.append(NSURLQueryItem(name: "email", value: account.email))
-                    viewController.url = components?.URL
-                case .NeedsPassword:
-                    let components = NSURLComponents(URL: profile.accountConfiguration.forceAuthURL, resolvingAgainstBaseURL: false)
-                    components?.queryItems?.append(NSURLQueryItem(name: "email", value: account.email))
-                    viewController.url = components?.URL
-                case .NeedsUpgrade:
-                    // In future, we'll want to link to an upgrade page.
-                    break
+            switch indexPath.item {
+            case ItemAccountStatus:
+                let viewController = FxAContentViewController()
+                viewController.delegate = self
+                if let account = profile.getAccount() {
+                    switch account.getActionNeeded() {
+                    case .None, .NeedsVerification:
+                        let cs = NSURLComponents(URL: profile.accountConfiguration.settingsURL, resolvingAgainstBaseURL: false)
+                        cs?.queryItems?.append(NSURLQueryItem(name: "email", value: account.email))
+                        viewController.url = cs?.URL
+                    case .NeedsPassword:
+                        let cs = NSURLComponents(URL: profile.accountConfiguration.forceAuthURL, resolvingAgainstBaseURL: false)
+                        cs?.queryItems?.append(NSURLQueryItem(name: "email", value: account.email))
+                        viewController.url = cs?.URL
+                    case .NeedsUpgrade:
+                        // In future, we'll want to link to an upgrade page.
+                        break
+                    }
+                } else {
+                    viewController.url = profile.accountConfiguration.signInURL
                 }
-            } else {
-                viewController.url = profile.accountConfiguration.signInURL
+                navigationController?.pushViewController(viewController, animated: true)
+
+            case ItemAccountDisconnect:
+                maybeDisconnectAccount()
+
+            default:
+                break
             }
-            navigationController?.pushViewController(viewController, animated: true)
         } else if indexPath.section == SectionSearch {
             let viewController = SearchSettingsTableViewController()
             viewController.model = profile.searchEngines
@@ -119,6 +143,23 @@ class SettingsTableViewController: UITableViewController {
         } else {
             cell.textLabel?.text = NSLocalizedString("Sign in", comment: "Settings")
         }
+    }
+
+    func maybeDisconnectAccount() {
+        let alertController = UIAlertController(
+            title: NSLocalizedString("Disconnect Firefox Account?", comment: "Settings"),
+            message: NSLocalizedString("Firefox will stop syncing with your account, but won’t delete any of your browsing data on this device.", comment: "Settings"),
+            preferredStyle: UIAlertControllerStyle.Alert)
+        alertController.addAction(
+            UIAlertAction(title: NSLocalizedString("Cancel", comment: "Settings"), style: .Cancel) { (action) in
+                // Do nothing.
+            })
+        alertController.addAction(
+            UIAlertAction(title: NSLocalizedString("Disconnect", comment: "Settings"), style: .Destructive) { (action) in
+                self.profile.setAccount(nil)
+                self.tableView.reloadData()
+            })
+        presentViewController(alertController, animated: true, completion: nil)
     }
 }
 