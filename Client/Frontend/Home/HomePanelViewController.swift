mralext20/firefox-ios

Fixes 1171599 - Remote Tabs Home Panel needs a 'not logged in' state

Client/Frontend/Home/HomePanelViewController.swift
@@ -17,6 +17,8 @@ private struct HomePanelViewControllerUX {
 protocol HomePanelViewControllerDelegate: class {
     func homePanelViewController(homePanelViewController: HomePanelViewController, didSelectURL url: NSURL, visitType: VisitType)
     func homePanelViewController(HomePanelViewController: HomePanelViewController, didSelectPanel panel: Int)
+    func homePanelViewControllerDidRequestToSignIn(homePanelViewController: HomePanelViewController)
+    func homePanelViewControllerDidRequestToCreateAccount(homePanelViewController: HomePanelViewController)
 }
 
 @objc
@@ -26,6 +28,8 @@ protocol HomePanel: class {
 
 @objc
 protocol HomePanelDelegate: class {
+    func homePanelDidRequestToSignIn(remoteTabsPanel: RemoteTabsPanel)
+    func homePanelDidRequestToCreateAccount(remoteTabsPanel: RemoteTabsPanel)
     func homePanel(homePanel: HomePanel, didSelectURL url: NSURL, visitType: VisitType)
 }
 
@@ -182,5 +186,13 @@ class HomePanelViewController: UIViewController, UITextFieldDelegate, HomePanelD
     func homePanel(homePanel: HomePanel, didSelectURL url: NSURL, visitType: VisitType) {
         delegate?.homePanelViewController(self, didSelectURL: url, visitType: visitType)
         dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    func homePanelDidRequestToCreateAccount(remoteTabsPanel: RemoteTabsPanel) {
+        delegate?.homePanelViewControllerDidRequestToCreateAccount(self)
+    }
+
+    func homePanelDidRequestToSignIn(remoteTabsPanel: RemoteTabsPanel) {
+        delegate?.homePanelViewControllerDidRequestToSignIn(self)
     }
 }

ecosia/ios-browser

Client/Frontend/Home/HomePanelViewController.swift
@@ -17,6 +17,8 @@ private struct HomePanelViewControllerUX {
 protocol HomePanelViewControllerDelegate: class {
     func homePanelViewController(homePanelViewController: HomePanelViewController, didSelectURL url: NSURL, visitType: VisitType)
     func homePanelViewController(HomePanelViewController: HomePanelViewController, didSelectPanel panel: Int)
+    func homePanelViewControllerDidRequestToSignIn(homePanelViewController: HomePanelViewController)
+    func homePanelViewControllerDidRequestToCreateAccount(homePanelViewController: HomePanelViewController)
 }
 
 @objc
@@ -26,6 +28,8 @@ protocol HomePanel: class {
 
 @objc
 protocol HomePanelDelegate: class {
+    func homePanelDidRequestToSignIn(homePanel: HomePanel)
+    func homePanelDidRequestToCreateAccount(homePanel: HomePanel)
     func homePanel(homePanel: HomePanel, didSelectURL url: NSURL, visitType: VisitType)
 }
 
@@ -181,5 +185,13 @@ class HomePanelViewController: UIViewController, UITextFieldDelegate, HomePanelD
     func homePanel(homePanel: HomePanel, didSelectURL url: NSURL, visitType: VisitType) {
         delegate?.homePanelViewController(self, didSelectURL: url, visitType: visitType)
         dismissViewControllerAnimated(true, completion: nil)
+    }
+
+    func homePanelDidRequestToCreateAccount(homePanel: HomePanel) {
+        delegate?.homePanelViewControllerDidRequestToCreateAccount(self)
+    }
+
+    func homePanelDidRequestToSignIn(homePanel: HomePanel) {
+        delegate?.homePanelViewControllerDidRequestToSignIn(self)
     }
 }