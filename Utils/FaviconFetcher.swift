mozilla-mobile/firefox-ios

Use alamofire with correct useragent for loading pages

Utils/FaviconFetcher.swift
@@ -1,6 +1,9 @@
 import Storage
 import Shared
+import Alamofire
+import XCGLogger
 
+private let log = XCGLogger.defaultInstance()
 private let queue = dispatch_queue_create("FaviconFetcher", DISPATCH_QUEUE_CONCURRENT)
 
 class FaviconFetcherErrorType: ErrorType {
@@ -13,108 +16,119 @@ class FaviconFetcherErrorType: ErrorType {
 /* A helper class to find the favicon associated with a url. This will load the page and parse any icons it finds out of it
  * If that fails, it will attempt to find a favicon.ico in the root host domain
  */
-class FaviconFetcher : NSObject, NSXMLParserDelegate {
+public class FaviconFetcher : NSObject, NSXMLParserDelegate {
+    public static var userAgent: String = ""
     static let ExpirationTime = NSTimeInterval(60*60*24*7) // Only check for icons once a week
 
-    private var siteUrl: NSURL // The url we're looking for favicons for
-    private var _favicons = [Favicon]() // An internal cache of favicons found for this url
-
     class func getForUrl(url: NSURL, profile: Profile) -> Deferred<Result<[Favicon]>> {
-        let f = FaviconFetcher(url: url)
-        return f.loadFavicons(profile)
+        let f = FaviconFetcher()
+        return f.loadFavicons(url, profile: profile)
     }
 
-    private init(url: NSURL) {
-        siteUrl = url
-    }
+    private var attemptedSubdomains = [String]()
 
-    private func loadFavicons(profile: Profile) -> Deferred<Result<[Favicon]>> {
+    private func loadFavicons(url: NSURL, profile: Profile, var oldIcons: [Favicon] = [Favicon]()) -> Deferred<Result<[Favicon]>> {
+        // log.debug("Loading \(url)")
         let deferred = Deferred<Result<[Favicon]>>()
 
         dispatch_async(queue) { _ in
-            if self._favicons.count == 0 {
-                // Initially look for tags in the page
-                self.loadFromDoc()
-            }
-
-            // If that didn't find anything, look for a favicon.ico for this host
-            if self._favicons.count == 0 {
-                self.loadFromHost()
-            }
-
-            var filledCount = 0
-            for (i, icon) in enumerate(self._favicons) {
-                // For each icon we set of an async load of the data (in order to get the width/height.
-                self.getFavicon(icon, profile: profile).upon { result in
+            self.loadFromLinks(url).bind({ (result: Result<[Favicon]>) -> Deferred<[Result<Favicon>]> in
+                var deferreds = [Deferred<Result<Favicon>>]()
+                if let icons = result.successValue {
+                    deferreds = map(icons) { self.getFavicon(url, icon: $0, profile: profile) }
+                }
+                return all(deferreds)
+            }).bind({ (results: [Result<Favicon>]) -> Deferred<Result<[Favicon]>> in
+                for result in results {
                     if let icon = result.successValue {
-                        self._favicons[i] = icon
-                    }
-                    filledCount++
-
-                    // When they've all completed, we can fill the deferred with the results
-                    if filledCount == self._favicons.count {
-                        self._favicons.sort({ (a, b) -> Bool in
-                            return a.width > b.width
-                        })
-
-                        deferred.fill(Result(success: self._favicons))
+                        oldIcons.append(icon)
                     }
                 }
-            }
+
+                oldIcons.sort({ (a, b) -> Bool in
+                    return a.width > b.width
+                })
+
+                return deferResult(oldIcons)
+            }).upon({ (result: Result<[Favicon]>) in
+                deferred.fill(result)
+                return
+            })
         }
 
         return deferred
     }
 
-    // Loads favicon.ico on the host domain for this url
-    private func loadFromHost() {
-        if let url = NSURL(scheme: siteUrl.scheme!, host: siteUrl.host, path: "/favicon.ico") {
-            let icon = Favicon(url: url.absoluteString!, type: IconType.Guess)
-            _favicons.append(icon)
+    lazy private var alamofire: Alamofire.Manager = {
+        var defaultHeaders = Alamofire.Manager.sharedInstance.session.configuration.HTTPAdditionalHeaders ?? [:]
+        defaultHeaders["User-Agent"] = userAgent
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        configuration.timeoutIntervalForRequest = 5
+        configuration.HTTPAdditionalHeaders = defaultHeaders
+        return Alamofire.Manager(configuration: configuration)
+    }()
+
+    private func fetchDataForUrl(url: NSURL) -> Deferred<Result<NSData>> {
+        let deferred = Deferred<Result<NSData>>()
+        alamofire.request(.GET, url).response { (request, response, data, error) in
+            if let error = error {
+                deferred.fill(Result(failure: FaviconFetcherErrorType(description: error.description)))
+            } else {
+                let loc = response?.URL
+                deferred.fill(Result(success: data as! NSData))
+            }
         }
+        return deferred
     }
 
     // Loads and parses an html document and tries to find any known favicon-type tags for the page
-    private func loadFromDoc() {
+    private func loadFromLinks(url: NSURL) -> Deferred<Result<[Favicon]>> {
         var err: NSError?
 
-        if let data = NSData(contentsOfURL: siteUrl),
-           let element = RXMLElement(fromHTMLData: data) {
-            element.iterate("head.meta") { meta in
-                if let refresh = meta.attribute("http-equiv"),
-                   let content = meta.attribute("content"),
-                   let index = content.rangeOfString("URL="),
-                   let url = NSURL(string: content.substringFromIndex(advance(index.startIndex,4))) {
-                    if refresh == "Refresh" {
-                        self.siteUrl = url
-                        self.loadFromDoc()
-                        return
+        return fetchDataForUrl(url).bind({ result -> Deferred<Result<[Favicon]>> in
+            var icons = [Favicon]()
+
+            if let data = result.successValue,
+               let element = RXMLElement(fromHTMLData: data) {
+                var reloadUrl: NSURL? = nil
+                element.iterate("head.meta") { meta in
+                    if let refresh = meta.attribute("http-equiv") where refresh == "Refresh",
+                        let content = meta.attribute("content"),
+                        let index = content.rangeOfString("URL="),
+                        let url = NSURL(string: content.substringFromIndex(advance(index.startIndex,4))) {
+                            reloadUrl = url
+                    }
+                }
+
+                if let url = reloadUrl {
+                    return self.loadFromLinks(url)
+                }
+
+                element.iterate("head.link") { link in
+                    if let rel = link.attribute("rel") where (rel == "shortcut icon" || rel == "icon" || rel == "apple-touch-icon"),
+                        let href = link.attribute("href"),
+                        let url = NSURL(string: href, relativeToURL: url) {
+                            let icon = Favicon(url: url.absoluteString!, date: NSDate(), type: IconType.Icon)
+                            icons.append(icon)
                     }
                 }
             }
 
-            element.iterate("head.link") { link in
-                if var rel = link.attribute("rel") where (rel == "shortcut icon" || rel == "icon" || rel == "apple-touch-icon"),
-                    var href = link.attribute("href"),
-                    var url = NSURL(string: href, relativeToURL: self.siteUrl) {
-                        let icon = Favicon(url: url.absoluteString!, date: NSDate(), type: IconType.Icon)
-                        self._favicons.append(icon)
-                }
-            }
-        }
+            return deferResult(icons)
+        })
     }
 
-    private func getFavicon(icon: Favicon, profile: Profile) -> Deferred<Result<Favicon>> {
+    private func getFavicon(siteUrl: NSURL, icon: Favicon, profile: Profile) -> Deferred<Result<Favicon>> {
         let deferred = Deferred<Result<Favicon>>()
         let url = icon.url
         let manager = SDWebImageManager.sharedManager()
         let site = Site(url: siteUrl.absoluteString!, title: "")
 
-        var fav = Favicon(url: url, type: IconType.Icon)
+        var fav = Favicon(url: url, type: icon.type)
         if let url = url.asURL {
             manager.downloadImageWithURL(url, options: SDWebImageOptions.LowPriority, progress: nil, completed: { (img, err, cacheType, success, url) -> Void in
                 fav = Favicon(url: url.absoluteString!,
-                    type: IconType.Icon)
+                    type: icon.type)
 
                 if let img = img {
                     fav.width = Int(img.size.width)

ecosia/ios-browser

Utils/FaviconFetcher.swift
@@ -1,6 +1,9 @@
 import Storage
 import Shared
+import Alamofire
+import XCGLogger
 
+private let log = XCGLogger.defaultInstance()
 private let queue = dispatch_queue_create("FaviconFetcher", DISPATCH_QUEUE_CONCURRENT)
 
 class FaviconFetcherErrorType: ErrorType {
@@ -13,108 +16,118 @@ class FaviconFetcherErrorType: ErrorType {
 /* A helper class to find the favicon associated with a url. This will load the page and parse any icons it finds out of it
  * If that fails, it will attempt to find a favicon.ico in the root host domain
  */
-class FaviconFetcher : NSObject, NSXMLParserDelegate {
+public class FaviconFetcher : NSObject, NSXMLParserDelegate {
+    public static var userAgent: String = ""
     static let ExpirationTime = NSTimeInterval(60*60*24*7) // Only check for icons once a week
 
-    private var siteUrl: NSURL // The url we're looking for favicons for
-    private var _favicons = [Favicon]() // An internal cache of favicons found for this url
-
     class func getForUrl(url: NSURL, profile: Profile) -> Deferred<Result<[Favicon]>> {
-        let f = FaviconFetcher(url: url)
-        return f.loadFavicons(profile)
+        let f = FaviconFetcher()
+        return f.loadFavicons(url, profile: profile)
     }
 
-    private init(url: NSURL) {
-        siteUrl = url
-    }
-
-    private func loadFavicons(profile: Profile) -> Deferred<Result<[Favicon]>> {
+    private func loadFavicons(url: NSURL, profile: Profile, var oldIcons: [Favicon] = [Favicon]()) -> Deferred<Result<[Favicon]>> {
         let deferred = Deferred<Result<[Favicon]>>()
 
         dispatch_async(queue) { _ in
-            if self._favicons.count == 0 {
-                // Initially look for tags in the page
-                self.loadFromDoc()
-            }
-
-            // If that didn't find anything, look for a favicon.ico for this host
-            if self._favicons.count == 0 {
-                self.loadFromHost()
-            }
-
-            var filledCount = 0
-            for (i, icon) in enumerate(self._favicons) {
-                // For each icon we set of an async load of the data (in order to get the width/height.
-                self.getFavicon(icon, profile: profile).upon { result in
+            self.parseHTMLForFavicons(url).bind({ (result: Result<[Favicon]>) -> Deferred<[Result<Favicon>]> in
+                var deferreds = [Deferred<Result<Favicon>>]()
+                if let icons = result.successValue {
+                    deferreds = map(icons) { self.getFavicon(url, icon: $0, profile: profile) }
+                }
+                return all(deferreds)
+            }).bind({ (results: [Result<Favicon>]) -> Deferred<Result<[Favicon]>> in
+                for result in results {
                     if let icon = result.successValue {
-                        self._favicons[i] = icon
-                    }
-                    filledCount++
-
-                    // When they've all completed, we can fill the deferred with the results
-                    if filledCount == self._favicons.count {
-                        self._favicons.sort({ (a, b) -> Bool in
-                            return a.width > b.width
-                        })
-
-                        deferred.fill(Result(success: self._favicons))
+                        oldIcons.append(icon)
                     }
                 }
-            }
+
+                oldIcons.sort({ (a, b) -> Bool in
+                    return a.width > b.width
+                })
+
+                return deferResult(oldIcons)
+            }).upon({ (result: Result<[Favicon]>) in
+                deferred.fill(result)
+                return
+            })
         }
 
         return deferred
     }
 
-    // Loads favicon.ico on the host domain for this url
-    private func loadFromHost() {
-        if let url = NSURL(scheme: siteUrl.scheme!, host: siteUrl.host, path: "/favicon.ico") {
-            let icon = Favicon(url: url.absoluteString!, type: IconType.Guess)
-            _favicons.append(icon)
+    lazy private var alamofire: Alamofire.Manager = {
+        var defaultHeaders = Alamofire.Manager.sharedInstance.session.configuration.HTTPAdditionalHeaders ?? [:]
+        defaultHeaders["User-Agent"] = userAgent
+
+        let configuration = NSURLSessionConfiguration.defaultSessionConfiguration()
+        configuration.timeoutIntervalForRequest = 5
+        configuration.HTTPAdditionalHeaders = defaultHeaders
+
+        return Alamofire.Manager(configuration: configuration)
+    }()
+
+    private func fetchDataForUrl(url: NSURL) -> Deferred<Result<NSData>> {
+        let deferred = Deferred<Result<NSData>>()
+        alamofire.request(.GET, url).response { (request, response, data, error) in
+            if let error = error {
+                deferred.fill(Result(failure: FaviconFetcherErrorType(description: error.description)))
+            } else {
+                let loc = response?.URL
+                deferred.fill(Result(success: data as! NSData))
+            }
         }
+        return deferred
     }
 
     // Loads and parses an html document and tries to find any known favicon-type tags for the page
-    private func loadFromDoc() {
+    private func parseHTMLForFavicons(url: NSURL) -> Deferred<Result<[Favicon]>> {
         var err: NSError?
 
-        if let data = NSData(contentsOfURL: siteUrl),
-           let element = RXMLElement(fromHTMLData: data) {
-            element.iterate("head.meta") { meta in
-                if let refresh = meta.attribute("http-equiv"),
-                   let content = meta.attribute("content"),
-                   let index = content.rangeOfString("URL="),
-                   let url = NSURL(string: content.substringFromIndex(advance(index.startIndex,4))) {
-                    if refresh == "Refresh" {
-                        self.siteUrl = url
-                        self.loadFromDoc()
-                        return
+        return fetchDataForUrl(url).bind({ result -> Deferred<Result<[Favicon]>> in
+            var icons = [Favicon]()
+
+            if let data = result.successValue,
+               let element = RXMLElement(fromHTMLData: data) {
+                var reloadUrl: NSURL? = nil
+                element.iterate("head.meta") { meta in
+                    if let refresh = meta.attribute("http-equiv") where refresh == "Refresh",
+                        let content = meta.attribute("content"),
+                        let index = content.rangeOfString("URL="),
+                        let url = NSURL(string: content.substringFromIndex(advance(index.startIndex,4))) {
+                            reloadUrl = url
+                    }
+                }
+
+                if let url = reloadUrl {
+                    return self.parseHTMLForFavicons(url)
+                }
+
+                element.iterate("head.link") { link in
+                    if let rel = link.attribute("rel") where (rel == "shortcut icon" || rel == "icon" || rel == "apple-touch-icon"),
+                        let href = link.attribute("href"),
+                        let url = NSURL(string: href, relativeToURL: url) {
+                            let icon = Favicon(url: url.absoluteString!, date: NSDate(), type: IconType.Icon)
+                            icons.append(icon)
                     }
                 }
             }
 
-            element.iterate("head.link") { link in
-                if var rel = link.attribute("rel") where (rel == "shortcut icon" || rel == "icon" || rel == "apple-touch-icon"),
-                    var href = link.attribute("href"),
-                    var url = NSURL(string: href, relativeToURL: self.siteUrl) {
-                        let icon = Favicon(url: url.absoluteString!, date: NSDate(), type: IconType.Icon)
-                        self._favicons.append(icon)
-                }
-            }
-        }
+            return deferResult(icons)
+        })
     }
 
-    private func getFavicon(icon: Favicon, profile: Profile) -> Deferred<Result<Favicon>> {
+    private func getFavicon(siteUrl: NSURL, icon: Favicon, profile: Profile) -> Deferred<Result<Favicon>> {
         let deferred = Deferred<Result<Favicon>>()
         let url = icon.url
         let manager = SDWebImageManager.sharedManager()
         let site = Site(url: siteUrl.absoluteString!, title: "")
 
-        var fav = Favicon(url: url, type: IconType.Icon)
+        var fav = Favicon(url: url, type: icon.type)
         if let url = url.asURL {
             manager.downloadImageWithURL(url, options: SDWebImageOptions.LowPriority, progress: nil, completed: { (img, err, cacheType, success, url) -> Void in
                 fav = Favicon(url: url.absoluteString!,
-                    type: IconType.Icon)
+                    type: icon.type)
 
                 if let img = img {
                     fav.width = Int(img.size.width)