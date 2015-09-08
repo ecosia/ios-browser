mozilla-mobile/firefox-ios

Bug 1201642 - Allow clearing individual private data items

Client/Frontend/Settings/ClearPrivateDataTableViewController.swift
@@ -1,0 +1,130 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import UIKit
+import Shared
+
+private let SectionToggles = 0
+private let SectionButton = 1
+private let NumberOfSections = 2
+private let SectionHeaderIdentifier = "SectionHeaderIdentifier"
+private let HeaderHeight: CGFloat = 44
+
+class ClearPrivateDataTableViewController: UITableViewController {
+    private var clearButton: UITableViewCell?
+
+    var profile: Profile!
+    var tabManager: TabManager!
+
+    private lazy var clearables: [Clearable] = {
+        return [
+            HistoryClearable(profile: self.profile),
+            CacheClearable(tabManager: self.tabManager),
+            CookiesClearable(tabManager: self.tabManager),
+            SiteDataClearable(tabManager: self.tabManager),
+            PasswordsClearable(profile: self.profile),
+        ]
+    }()
+
+    private lazy var toggledIndices: [Bool] = {
+        return [Bool](count: self.clearables.count, repeatedValue: true)
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = NSLocalizedString("Clear Private Data", comment: "Navigation title for clearing private data.")
+
+        tableView.registerClass(SettingsTableSectionHeaderView.self, forHeaderFooterViewReuseIdentifier: SectionHeaderIdentifier)
+
+        tableView.separatorColor = UIConstants.TableViewSeparatorColor
+        tableView.backgroundColor = UIConstants.TableViewHeaderBackgroundColor
+
+        tableView.tableFooterView = UIView()
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+
+        if indexPath.section == SectionToggles {
+            cell.textLabel?.text = clearables[indexPath.item].label
+            let control = UISwitch()
+            control.onTintColor = UIConstants.ControlTintColor
+            control.addTarget(self, action: "switchValueChanged:", forControlEvents: UIControlEvents.ValueChanged)
+            control.on = true
+            cell.accessoryView = control
+            cell.selectionStyle = .None
+            control.tag = indexPath.item
+        } else {
+            assert(indexPath.section == SectionButton)
+            cell.textLabel?.text = NSLocalizedString("Clear Private Data", comment: "Button in settings that clears private data for the selected items.")
+            cell.textLabel?.textAlignment = NSTextAlignment.Center
+            cell.textLabel?.textColor = UIConstants.DestructiveRed
+            clearButton = cell
+        }
+
+        // Make the separator line fill the entire table width.
+        cell.separatorInset = UIEdgeInsetsZero
+
+        return cell
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return NumberOfSections
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == SectionToggles {
+            return clearables.count
+        }
+
+        assert(section == SectionButton)
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, shouldHighlightRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        guard indexPath.section == SectionButton else { return false }
+
+        // Highlight the button only if at least one clearable is enabled.
+        return toggledIndices.contains(true)
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        guard indexPath.section == SectionButton else { return }
+
+        clearables
+            .enumerate()
+            .filter { (i, _) in toggledIndices[i] }
+            .map { (_, clearable) in clearable.clear() }
+            .allSucceed()
+            .upon { result in
+                // TODO: Need some kind of success/failure UI. Bug 1202093.
+                if result.isSuccess {
+                    print("Private data cleared")
+                } else {
+                    print("Error clearing private data")
+                    assertionFailure("\(result.failureValue)")
+                }
+
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.navigationController?.popViewControllerAnimated(true)
+                }
+        }
+    }
+
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return tableView.dequeueReusableHeaderFooterViewWithIdentifier(SectionHeaderIdentifier) as! SettingsTableSectionHeaderView
+    }
+
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return HeaderHeight
+    }
+
+    @objc func switchValueChanged(toggle: UISwitch) {
+        toggledIndices[toggle.tag] = toggle.on
+
+        // Dim the clear button if no clearables are selected.
+        clearButton?.textLabel?.textColor = toggledIndices.contains(true) ? UIConstants.DestructiveRed : UIColor.lightGrayColor()
+    }
+}

Client/Frontend/Settings/Clearables.swift
@@ -8,6 +8,7 @@ import Shared
 // A base protocol for something that can be cleared.
 protocol Clearable {
     func clear() -> Success
+    var label: String { get }
 }
 
 class ClearableError: MaybeErrorType {
@@ -26,6 +27,10 @@ class HistoryClearable: Clearable {
         self.profile = profile
     }
 
+    var label: String {
+        return NSLocalizedString("Browsing data", comment: "Settings item for clearing browsing history")
+    }
+
     func clear() -> Success {
         return profile.history.clearHistory().bind { success in
             SDImageCache.sharedImageCache().clearDisk()
@@ -42,6 +47,10 @@ class PasswordsClearable: Clearable {
     let profile: Profile
     init(profile: Profile) {
         self.profile = profile
+    }
+
+    var label: String {
+        return NSLocalizedString("Saved logins", comment: "Settings item for clearing passwords and login data")
     }
 
     func clear() -> Success {
@@ -77,6 +86,10 @@ class CacheClearable: Clearable {
     let tabManager: TabManager
     init(tabManager: TabManager) {
         self.tabManager = tabManager
+    }
+
+    var label: String {
+        return NSLocalizedString("Cache", comment: "Settings item for clearing the cache")
     }
 
     func clear() -> Success {
@@ -124,6 +137,10 @@ class SiteDataClearable : Clearable {
         self.tabManager = tabManager
     }
 
+    var label: String {
+        return NSLocalizedString("Offline website data", comment: "Settings item for clearing website data")
+    }
+
     func clear() -> Success {
         // First, close all tabs to make sure they don't hold any thing in memory.
         tabManager.removeAll()
@@ -146,6 +163,10 @@ class CookiesClearable: Clearable {
         self.tabManager = tabManager
     }
 
+    var label: String {
+        return NSLocalizedString("Cookies", comment: "Settings item for clearing cookies")
+    }
+
     func clear() -> Success {
         // First close all tabs to make sure they aren't holding anything in memory.
         tabManager.removeAll()
@@ -166,30 +187,5 @@ class CookiesClearable: Clearable {
         }
 
         return succeed()
-    }
-}
-
-// A Clearable designed to clear all of the locally stored data for our app.
-class EverythingClearable: Clearable {
-    private let clearables: [Clearable]
-
-    init(profile: Profile, tabmanager: TabManager) {
-        clearables = [
-            HistoryClearable(profile: profile),
-            CacheClearable(tabManager: tabmanager),
-            CookiesClearable(tabManager: tabmanager),
-            SiteDataClearable(tabManager: tabmanager),
-            PasswordsClearable(profile: profile),
-        ]
-    }
-
-    func clear() -> Success {
-        let deferred = Success()
-        all(clearables.map({ clearable in
-            clearable.clear()
-        })).upon({ result in
-            deferred.fill(Maybe(success: ()))
-        })
-        return deferred
     }
 }

ecosia/ios-browser

Client/Frontend/Settings/ClearPrivateDataTableViewController.swift
@@ -1,0 +1,130 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import UIKit
+import Shared
+
+private let SectionToggles = 0
+private let SectionButton = 1
+private let NumberOfSections = 2
+private let SectionHeaderIdentifier = "SectionHeaderIdentifier"
+private let HeaderHeight: CGFloat = 44
+
+class ClearPrivateDataTableViewController: UITableViewController {
+    private var clearButton: UITableViewCell?
+
+    var profile: Profile!
+    var tabManager: TabManager!
+
+    private lazy var clearables: [Clearable] = {
+        return [
+            HistoryClearable(profile: self.profile),
+            CacheClearable(tabManager: self.tabManager),
+            CookiesClearable(tabManager: self.tabManager),
+            SiteDataClearable(tabManager: self.tabManager),
+            PasswordsClearable(profile: self.profile),
+        ]
+    }()
+
+    private lazy var toggles: [Bool] = {
+        return [Bool](count: self.clearables.count, repeatedValue: true)
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = NSLocalizedString("Clear Private Data", comment: "Navigation title for clearing private data.")
+
+        tableView.registerClass(SettingsTableSectionHeaderView.self, forHeaderFooterViewReuseIdentifier: SectionHeaderIdentifier)
+
+        tableView.separatorColor = UIConstants.TableViewSeparatorColor
+        tableView.backgroundColor = UIConstants.TableViewHeaderBackgroundColor
+
+        tableView.tableFooterView = UIView()
+    }
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: nil)
+
+        if indexPath.section == SectionToggles {
+            cell.textLabel?.text = clearables[indexPath.item].label
+            let control = UISwitch()
+            control.onTintColor = UIConstants.ControlTintColor
+            control.addTarget(self, action: "switchValueChanged:", forControlEvents: UIControlEvents.ValueChanged)
+            control.on = true
+            cell.accessoryView = control
+            cell.selectionStyle = .None
+            control.tag = indexPath.item
+        } else {
+            assert(indexPath.section == SectionButton)
+            cell.textLabel?.text = NSLocalizedString("Clear Private Data", comment: "Button in settings that clears private data for the selected items.")
+            cell.textLabel?.textAlignment = NSTextAlignment.Center
+            cell.textLabel?.textColor = UIConstants.DestructiveRed
+            clearButton = cell
+        }
+
+        // Make the separator line fill the entire table width.
+        cell.separatorInset = UIEdgeInsetsZero
+
+        return cell
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return NumberOfSections
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if section == SectionToggles {
+            return clearables.count
+        }
+
+        assert(section == SectionButton)
+        return 1
+    }
+
+    override func tableView(tableView: UITableView, shouldHighlightRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        guard indexPath.section == SectionButton else { return false }
+
+        // Highlight the button only if at least one clearable is enabled.
+        return toggles.contains(true)
+    }
+
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        guard indexPath.section == SectionButton else { return }
+
+        clearables
+            .enumerate()
+            .filter { (i, _) in toggles[i] }
+            .map { (_, clearable) in clearable.clear() }
+            .allSucceed()
+            .upon { result in
+                // TODO: Need some kind of success/failure UI. Bug 1202093.
+                if result.isSuccess {
+                    print("Private data cleared")
+                } else {
+                    print("Error clearing private data")
+                    assertionFailure("\(result.failureValue)")
+                }
+
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.navigationController?.popViewControllerAnimated(true)
+                }
+        }
+    }
+
+    override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        return tableView.dequeueReusableHeaderFooterViewWithIdentifier(SectionHeaderIdentifier) as! SettingsTableSectionHeaderView
+    }
+
+    override func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return HeaderHeight
+    }
+
+    @objc func switchValueChanged(toggle: UISwitch) {
+        toggles[toggle.tag] = toggle.on
+
+        // Dim the clear button if no clearables are selected.
+        clearButton?.textLabel?.textColor = toggles.contains(true) ? UIConstants.DestructiveRed : UIColor.lightGrayColor()
+    }
+}

Client/Frontend/Settings/Clearables.swift
@@ -8,6 +8,7 @@ import Shared
 // A base protocol for something that can be cleared.
 protocol Clearable {
     func clear() -> Success
+    var label: String { get }
 }
 
 class ClearableError: MaybeErrorType {
@@ -26,6 +27,10 @@ class HistoryClearable: Clearable {
         self.profile = profile
     }
 
+    var label: String {
+        return NSLocalizedString("Browsing History", comment: "Settings item for clearing browsing history")
+    }
+
     func clear() -> Success {
         return profile.history.clearHistory().bind { success in
             SDImageCache.sharedImageCache().clearDisk()
@@ -42,6 +47,10 @@ class PasswordsClearable: Clearable {
     let profile: Profile
     init(profile: Profile) {
         self.profile = profile
+    }
+
+    var label: String {
+        return NSLocalizedString("Saved Logins", comment: "Settings item for clearing passwords and login data")
     }
 
     func clear() -> Success {
@@ -77,6 +86,10 @@ class CacheClearable: Clearable {
     let tabManager: TabManager
     init(tabManager: TabManager) {
         self.tabManager = tabManager
+    }
+
+    var label: String {
+        return NSLocalizedString("Cache", comment: "Settings item for clearing the cache")
     }
 
     func clear() -> Success {
@@ -124,6 +137,10 @@ class SiteDataClearable : Clearable {
         self.tabManager = tabManager
     }
 
+    var label: String {
+        return NSLocalizedString("Offline Website Data", comment: "Settings item for clearing website data")
+    }
+
     func clear() -> Success {
         // First, close all tabs to make sure they don't hold any thing in memory.
         tabManager.removeAll()
@@ -146,6 +163,10 @@ class CookiesClearable: Clearable {
         self.tabManager = tabManager
     }
 
+    var label: String {
+        return NSLocalizedString("Cookies", comment: "Settings item for clearing cookies")
+    }
+
     func clear() -> Success {
         // First close all tabs to make sure they aren't holding anything in memory.
         tabManager.removeAll()
@@ -166,30 +187,5 @@ class CookiesClearable: Clearable {
         }
 
         return succeed()
-    }
-}
-
-// A Clearable designed to clear all of the locally stored data for our app.
-class EverythingClearable: Clearable {
-    private let clearables: [Clearable]
-
-    init(profile: Profile, tabmanager: TabManager) {
-        clearables = [
-            HistoryClearable(profile: profile),
-            CacheClearable(tabManager: tabmanager),
-            CookiesClearable(tabManager: tabmanager),
-            SiteDataClearable(tabManager: tabmanager),
-            PasswordsClearable(profile: profile),
-        ]
-    }
-
-    func clear() -> Success {
-        let deferred = Success()
-        all(clearables.map({ clearable in
-            clearable.clear()
-        })).upon({ result in
-            deferred.fill(Maybe(success: ()))
-        })
-        return deferred
     }
 }