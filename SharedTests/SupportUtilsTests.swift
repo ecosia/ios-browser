mystic123/firefox-ios

Bug 1232188 - Adjust Retention - Part 1: Utility class to create links to SUMO

SharedTests/SupportUtilsTests.swift
@@ -1,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import Shared
+import XCTest
+
+class SupportUtilsTests: XCTestCase {
+    func testLinkForTopic() {
+        let appVersion = AppInfo.appVersion
+        let localeIdentifier = NSLocale.currentLocale().localeIdentifier
+        XCTAssertEqual(SupportUtils.linkForTopic("Bacon")?.absoluteString, "https://support.mozilla.org/1/mobile/\(appVersion)/iOS/\(localeIdentifier)/Bacon")
+        XCTAssertEqual(SupportUtils.linkForTopic("Cheese & Crackers")?.absoluteString, "https://support.mozilla.org/1/mobile/\(appVersion)/iOS/\(localeIdentifier)/Cheese%20&%20Crackers")
+        XCTAssertEqual(SupportUtils.linkForTopic("Möbelträgerfüße")?.absoluteString, "https://support.mozilla.org/1/mobile/\(appVersion)/iOS/\(localeIdentifier)/M%C3%B6beltr%C3%A4gerf%C3%BC%C3%9Fe")
+    }
+}

Utils/SupportUtils.swift
@@ -1,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+
+/// Utility functions related to SUMO.
+public struct SupportUtils {
+    /// Construct a NSURL pointing to a specific topic on SUMO. The topic should be a non-escaped string. It will
+    /// be properly escaped by this function.
+    ///
+    /// The resulting link will include the app version, operating system and locale code. For example, a topic
+    /// "cheese" will be turned into a link that looks like https://support.mozilla.org/1/mobile/2.0/iOS/en-US/cheese
+    public static func linkForTopic(topic: String) -> NSURL? {
+        guard let escapedTopic = topic.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLPathAllowedCharacterSet()) else {
+            return nil
+        }
+        return NSURL(string: "https://support.mozilla.org/1/mobile/\(AppInfo.appVersion)/iOS/\(NSLocale.currentLocale().localeIdentifier)/\(escapedTopic)")
+    }
+}

ecosia/ios-browser

SharedTests/SupportUtilsTests.swift
@@ -1,0 +1,17 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+import Shared
+import XCTest
+
+class SupportUtilsTests: XCTestCase {
+    func testURLForTopic() {
+        let appVersion = AppInfo.appVersion
+        let localeIdentifier = NSLocale.currentLocale().localeIdentifier
+        XCTAssertEqual(SupportUtils.URLForTopic("Bacon")?.absoluteString, "https://support.mozilla.org/1/mobile/\(appVersion)/iOS/\(localeIdentifier)/Bacon")
+        XCTAssertEqual(SupportUtils.URLForTopic("Cheese & Crackers")?.absoluteString, "https://support.mozilla.org/1/mobile/\(appVersion)/iOS/\(localeIdentifier)/Cheese%20&%20Crackers")
+        XCTAssertEqual(SupportUtils.URLForTopic("Möbelträgerfüße")?.absoluteString, "https://support.mozilla.org/1/mobile/\(appVersion)/iOS/\(localeIdentifier)/M%C3%B6beltr%C3%A4gerf%C3%BC%C3%9Fe")
+    }
+}

Utils/SupportUtils.swift
@@ -1,0 +1,20 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import Foundation
+
+/// Utility functions related to SUMO.
+public struct SupportUtils {
+    /// Construct a NSURL pointing to a specific topic on SUMO. The topic should be a non-escaped string. It will
+    /// be properly escaped by this function.
+    ///
+    /// The resulting NSURL will include the app version, operating system and locale code. For example, a topic
+    /// "cheese" will be turned into a link that looks like https://support.mozilla.org/1/mobile/2.0/iOS/en-US/cheese
+    public static func URLForTopic(topic: String) -> NSURL? {
+        guard let escapedTopic = topic.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLPathAllowedCharacterSet()) else {
+            return nil
+        }
+        return NSURL(string: "https://support.mozilla.org/1/mobile/\(AppInfo.appVersion)/iOS/\(NSLocale.currentLocale().localeIdentifier)/\(escapedTopic)")
+    }
+}