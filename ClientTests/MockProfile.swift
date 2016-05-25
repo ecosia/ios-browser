egirsova/firefox-ios

created history back button widget and recent tabs panel class

ClientTests/MockProfile.swift
@@ -134,6 +134,10 @@ public class MockProfile: Profile {
         return ReadingListService(profileStoragePath: self.files.rootPath as String)
     }()
 
+    lazy var recentlyClosedTabs: RecentlyClosedTabs = {
+        return MockRecentlyClosedTabsStore(prefs: self.prefs)
+    }()
+
     internal lazy var remoteClientsAndTabs: RemoteClientsAndTabs = {
         return SQLiteRemoteClientsAndTabs(db: self.db)
     }()

Providers/Profile.swift
@@ -145,6 +145,7 @@ protocol Profile: class {
     var readingList: ReadingListService? { get }
     var logins: protocol<BrowserLogins, SyncableLogins, ResettableSyncStorage> { get }
     var certStore: CertStore { get }
+    var recentlyClosedTabs: RecentlyClosedTabs { get }
 
     func shutdown()
 
@@ -370,6 +371,10 @@ public class BrowserProfile: Profile {
 
     lazy var certStore: CertStore = {
         return CertStore()
+    }()
+
+    lazy var recentlyClosedTabs: RecentlyClosedTabs = {
+        return RecentlyClosedTabsStore(prefs: self.prefs)
     }()
 
     private func getSyncDelegate() -> SyncDelegate {

ecosia/ios-browser

ClientTests/MockProfile.swift
@@ -135,6 +135,10 @@ public class MockProfile: Profile {
         return ReadingListService(profileStoragePath: self.files.rootPath as String)
     }()
 
+    lazy var recentlyClosedTabs: ClosedTabsStore = {
+        return ClosedTabsStore(prefs: self.prefs)
+    }()
+
     internal lazy var remoteClientsAndTabs: RemoteClientsAndTabs = {
         return SQLiteRemoteClientsAndTabs(db: self.db)
     }()

Providers/Profile.swift
@@ -177,6 +177,7 @@ protocol Profile: class {
     var readingList: ReadingListService? { get }
     var logins: protocol<BrowserLogins, SyncableLogins, ResettableSyncStorage> { get }
     var certStore: CertStore { get }
+    var recentlyClosedTabs: ClosedTabsStore { get }
 
     func shutdown()
 
@@ -413,6 +414,10 @@ public class BrowserProfile: Profile {
 
     lazy var certStore: CertStore = {
         return CertStore()
+    }()
+
+    lazy var recentlyClosedTabs: ClosedTabsStore = {
+        return ClosedTabsStore(prefs: self.prefs)
     }()
 
     private func getSyncDelegate() -> SyncDelegate {