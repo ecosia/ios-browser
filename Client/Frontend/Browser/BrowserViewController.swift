mozilla-mobile/firefox-ios

commit 6fb111410b9afd78e8ed4a0eb9d184e729d21f8a

Bug 1413705 - Find in Page bar needs to respect safe area insets for iPhone X (#4344)

Client/Frontend/Browser/BrowserViewController.swift
@@ -668,11 +668,12 @@ class BrowserViewController: UIViewController {
 
         alertStackView.snp.remakeConstraints { make in
             make.centerX.equalTo(self.view)
-            make.width.equalTo(self.view.snp.width)
+            make.width.equalTo(self.view.safeArea.width)
             if let keyboardHeight = keyboardState?.intersectionHeightForView(self.view), keyboardHeight > 0 {
                 make.bottom.equalTo(self.view).offset(-keyboardHeight)
             } else if let toolbar = self.toolbar {
                 make.bottom.equalTo(toolbar.snp.top)
+                make.bottom.lessThanOrEqualTo(self.view.safeArea.bottom)
             } else {
                 make.bottom.equalTo(self.view)
             }

ecosia/ios-browser

commit 3a722e229a7be812d2ab1e4c3d867072c98740fc

Bug 1506517 - iPhone X toolbar hide/show not working (#4399)
Caused by earlier commit https://github.com/mozilla-mobile/firefox-ios/commit/6fb111410b9afd78e8ed4a0eb9d184e729d21f8a
Reverting the offending line and re-opening that bug.

Client/Frontend/Browser/BrowserViewController.swift
@@ -672,7 +672,6 @@ class BrowserViewController: UIViewController {
                 make.bottom.equalTo(self.view).offset(-keyboardHeight)
             } else if let toolbar = self.toolbar {
                 make.bottom.equalTo(toolbar.snp.top)
-                make.bottom.lessThanOrEqualTo(self.view.safeArea.bottom)
             } else {
                 make.bottom.equalTo(self.view)
             }