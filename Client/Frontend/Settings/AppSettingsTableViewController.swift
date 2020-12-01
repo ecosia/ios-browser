mozilla-mobile/firefox-ios

FXIOS-1195 ⁃ Fix #7308: Chron tabs a/b test (#7636)

Client/Frontend/Settings/AppSettingsTableViewController.swift
@@ -141,7 +141,8 @@ class AppSettingsTableViewController: SettingsTableViewController {
                 ShowEtpCoverSheet(settings: self),
                 ToggleOnboarding(settings: self),
                 LeanplumStatus(settings: self),
-                ClearOnboardingABVariables(settings: self)
+                ClearOnboardingABVariables(settings: self),
+                ToggleChronTabs(settings: self)
             ])]
 
         return settings

ecosia/ios-browser

Client/Frontend/Settings/AppSettingsTableViewController.swift
@@ -138,7 +138,8 @@ class AppSettingsTableViewController: SettingsTableViewController {
                 SentryIDSetting(settings: self),
                 ChangeToChinaSetting(settings: self),
                 ShowEtpCoverSheet(settings: self),
-                LeanplumStatus(settings: self)
+                LeanplumStatus(settings: self),
+                ToggleChronTabs(settings: self)
             ])]
 
         return settings