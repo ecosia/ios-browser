249af4738a9e69b5ee80500f9bd4943eda2dc342
vaux <zero.griffin@gmail.com>
2021-03-10 16:27:51 +0100

    Add promo events

--- Client/Frontend/Home/DefaultBrowserCard.swift
+++ Client/Frontend/Home/DefaultBrowserCard.swift
@@ -128,12 +128,22 @@ class DefaultBrowserCard: UIView {
         UserDefaults.standard.set(true, forKey: "DidDismissDefaultBrowserCard")
         TelemetryWrapper.gleanRecordEvent(category: .action, method: .tap, object: .dismissDefaultBrowserCard)
         LeanPlumClient.shared.track(event: .dismissDefaultBrowserCard)
+        
+        /*
+         Ecosia
+         */
+        Analytics.shared.defaultBrowser(.close)
     }
     
     @objc private func showSettings() {
         UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!, options: [:])
         TelemetryWrapper.gleanRecordEvent(category: .action, method: .tap, object: .goToSettingsDefaultBrowserCard)
         LeanPlumClient.shared.track(event: .goToSettingsDefaultBrowserCard)
+        
+        /*
+         Ecosia
+         */
+        Analytics.shared.defaultBrowser(.click)
     }
     
     func applyTheme() {
