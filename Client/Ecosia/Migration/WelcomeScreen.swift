62d0c561f321fcf0c9073e10390d2a8868058c45
vaux <zero.griffin@gmail.com>
2021-04-26 15:35:10 +0200

    Add welcome screen base

--- Client/Ecosia/Migration/WelcomeScreen.swift
+++ Client/Ecosia/Migration/WelcomeScreen.swift
@@ -15,5 +15,23 @@ final class WelcomeScreen: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor.Photon.Grey70.withAlphaComponent(0.4)
+        
+        let base = UIView()
+        base.translatesAutoresizingMaskIntoConstraints = false
+        base.clipsToBounds = true
+        base.backgroundColor = UIColor.theme.ecosia.primaryBackground
+        base.layer.cornerRadius = 8
+        view.addSubview(base)
+        
+        base.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        base.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        base.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        base.leftAnchor.constraint(greaterThanOrEqualTo: view.leftAnchor, constant: 16).isActive = true
+        base.rightAnchor.constraint(lessThanOrEqualTo: view.rightAnchor, constant: -16).isActive = true
+        base.widthAnchor.constraint(lessThanOrEqualToConstant: 360).isActive = true
+        
+        let width = base.widthAnchor.constraint(equalToConstant: 360)
+        width.priority = .defaultLow
+        width.isActive = true
     }
 }
