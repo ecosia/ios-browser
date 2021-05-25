a05d11c112544160657b6e97ad5030fcff54ed35
Jörn Ehmann <joern.ehmann@ecosia.org>
2021-05-25 12:21:29 +0200

    fix NTP layout to fit small screens

--- Client/Frontend/Home/FirefoxHomeTopSitesCell.swift
+++ Client/Frontend/Home/FirefoxHomeTopSitesCell.swift
@@ -45,7 +45,7 @@ class TopSiteItemCell: UICollectionViewCell, Themeable {
         let titleLabel = UILabel()
         titleLabel.layer.masksToBounds = true
         titleLabel.textAlignment = .center
-        titleLabel.font = TopSiteCellUX.TitleFont
+        titleLabel.font = .preferredFont(forTextStyle: .footnote)
         titleLabel.numberOfLines = 2
         titleLabel.adjustsFontSizeToFitWidth = true
         titleLabel.allowsDefaultTighteningForTruncation = true
@@ -94,7 +94,8 @@ class TopSiteItemCell: UICollectionViewCell, Themeable {
         titleLabel.snp.makeConstraints { make in
             make.left.equalTo(contentView).offset(TopSiteCellUX.TitleOffset)
             make.right.equalTo(contentView).offset(-TopSiteCellUX.TitleOffset)
-            make.top.equalTo(faviconBG.snp.bottom).offset(12)
+            make.top.equalTo(faviconBG.snp.bottom).offset(8)
+            make.bottom.lessThanOrEqualTo(contentView)
         }
 
         imageView.snp.makeConstraints { make in
@@ -186,7 +187,7 @@ class TopSiteItemCell: UICollectionViewCell, Themeable {
         selectedOverlay.backgroundColor = TopSiteCellUX.OverlayColor
         titleBorder.backgroundColor = TopSiteCellUX.BorderColor.cgColor
         titleLabel.backgroundColor = UIColor.clear
-        titleLabel.textColor = UIColor.theme.homePanel.topSiteDomain
+        titleLabel.textColor = UIColor.theme.ecosia.highContrastText
     }
 }
 
