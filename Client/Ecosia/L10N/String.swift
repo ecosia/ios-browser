f4200a791cf0a8f4b58cb64f42281cd7ae1e3774
vaux <zero.griffin@gmail.com>
2021-09-20 11:58:45 +0200

    Add strings

--- Client/Ecosia/L10N/String.swift
+++ Client/Ecosia/L10N/String.swift
@@ -94,7 +94,6 @@ extension String {
         case showTopSites = "Show Top Sites"
         case everyTimeYouInvite = "Every time a friend accepts your invitation, you’ll contribute to plant extra trees."
         case successfulInvites = "%d successful invite(s)"
-        case plant3Trees = "Plant 3 trees"
         case invitingAFriend = "Inviting a friend"
         case shareYourInvitation = "Share your invitation link"
         case viaEmailText = "via email, text message or social media"
