2e4eb8e5ddb4db05601cfbf7205326bb867dc75b
Jörn Ehmann <joern.ehmann@ecosia.org>
2021-11-18 13:46:34 +0100

    Referrals NTP UI Fixes

--- Client/Ecosia/UI/EcosiaTheme.swift
+++ Client/Ecosia/UI/EcosiaTheme.swift
@@ -40,6 +40,7 @@ class EcosiaTheme {
     var barSeparator: UIColor { UIColor.Photon.Grey20 }
     var treeCountText: UIColor { UIColor(named: "emerald")! }
     var treeCountBackground: UIColor { UIColor(rgb: 0xE2F7F1) }
+    var ntpImpactBackground: UIColor { .white}
     var impactBackground: UIColor { UIColor.Photon.Grey10 }
     var impactMultiplyCardBackground: UIColor { .white }
     var impactMultiplyCardBorder: UIColor { .Photon.Grey20 }
@@ -74,7 +75,8 @@ final class DarkEcosiaTheme: EcosiaTheme {
     override var secondaryBrand: UIColor { .white }
     override var primaryBackground: UIColor { UIColor.Photon.Grey90 }
     override var treeCountText: UIColor { .white }
-    override var treeCountBackground: UIColor { UIColor.Photon.Grey80 }
+    override var treeCountBackground: UIColor { UIColor.Photon.Grey70 }
+    override var ntpImpactBackground: UIColor { UIColor.Photon.Grey80}
     override var impactBackground: UIColor { UIColor.Photon.Grey60 }
     override var impactMultiplyCardBackground: UIColor { .Photon.Grey70 }
     override var impactMultiplyCardBorder: UIColor { .Photon.Grey60 }
