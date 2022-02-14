d71d6b8e454e38e05a16e0433748e9659bab4ba4
vaux <zero.griffin@gmail.com>
2022-02-14 17:15:47 +0100

    Force refresh on multiply impact

--- Client/Ecosia/UI/MyImpact/EcosiaHome.swift
+++ Client/Ecosia/UI/MyImpact/EcosiaHome.swift
@@ -317,6 +317,7 @@ final class EcosiaHome: UICollectionViewController, UICollectionViewDelegateFlow
             dismiss(animated: true, completion: nil)
         case .multiply:
             navigationController?.pushViewController(MultiplyImpact(delegate: delegate), animated: true)
+            referrals.refresh()
         }
     }
 
