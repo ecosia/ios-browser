74a226cefa27e202a7d8b7f7024a9b13d1f0297f
vaux <zero.griffin@gmail.com>
2022-03-10 16:02:36 +0100

    Add semantic colors

--- Client/Ecosia/UI/SemanticColor.swift
+++ Client/Ecosia/UI/SemanticColor.swift
@@ -5,5 +5,21 @@
 import UIKit
 
 extension UIColor {
+    struct Light {
+        struct Background {
+            static let primary = UIColor.white
+            static let tertiary = UIColor(red: 0.941, green: 0.941, blue: 0.922, alpha: 1)
+        }
+        
+        static let border = UIColor(red: 0.871, green: 0.871, blue: 0.851, alpha: 1)
+    }
     
+    struct Dark {
+        struct Background {
+            static let primary = UIColor(red: 0.1, green: 0.1, blue: 0.1, alpha: 1)
+            static let secondary = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)
+        }
+        
+        static let border = UIColor(red: 0.35, green: 0.35, blue: 0.35, alpha: 1)
+    }
 }
