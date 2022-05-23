cdb30a1fce2c2cfadc89ed2b8bdfe13282f50810
vaux <zero.griffin@gmail.com>
2022-05-23 17:20:07 +0200

    Remove auto-imported swiftui framework

--- Client/Ecosia/UI/MyImpact/EcosiaExploreCell.swift
+++ Client/Ecosia/UI/MyImpact/EcosiaExploreCell.swift
@@ -3,7 +3,6 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
 import UIKit
-import SwiftUI
 
 final class EcosiaExploreCell: UICollectionViewCell, Themeable, AutoSizingCell {
     private(set) weak var widthConstraint: NSLayoutConstraint!
