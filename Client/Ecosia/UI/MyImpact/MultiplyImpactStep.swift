e4cb15b1de93488abe758333944374e6d0587a64
Jörn Ehmann <60604005+ecotopian@users.noreply.github.com>
2022-05-25 13:26:55 +0100

    Invite screen lower half (#277)
    * Bottom half of invite screen
    
    * add some fixes for iPad

--- Client/Ecosia/UI/MyImpact/MultiplyImpactStep.swift
+++ Client/Ecosia/UI/MyImpact/MultiplyImpactStep.swift
@@ -5,21 +5,20 @@
 import UIKit
 
 final class MultiplyImpactStep: UIView, Themeable {
-    private weak var indicator: UIView?
+    private weak var indicator: UIImageView?
     private weak var titleLabel: UILabel?
     private weak var subtitleLabel: UILabel?
     
     required init?(coder: NSCoder) { nil }
-    init(title: String, subtitle: String) {
+    init(title: String, subtitle: String, image: String) {
         super.init(frame: .zero)
         translatesAutoresizingMaskIntoConstraints = false
         isUserInteractionEnabled = false
         
-        let indicator = UIView()
+        let indicator = UIImageView(image: .init(named: image)?.withRenderingMode(.alwaysTemplate))
         indicator.translatesAutoresizingMaskIntoConstraints = false
         indicator.isUserInteractionEnabled = false
         indicator.clipsToBounds = true
-        indicator.layer.cornerRadius = 6
         addSubview(indicator)
         self.indicator = indicator
         
@@ -27,7 +26,7 @@ final class MultiplyImpactStep: UIView, Themeable {
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         titleLabel.numberOfLines = 0
         titleLabel.text = title
-        titleLabel.font = .preferredFont(forTextStyle: .subheadline)
+        titleLabel.font = .preferredFont(forTextStyle: .subheadline).bold()
         titleLabel.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
         titleLabel.adjustsFontForContentSizeCategory = true
         addSubview(titleLabel)
@@ -45,22 +44,22 @@ final class MultiplyImpactStep: UIView, Themeable {
         
         bottomAnchor.constraint(equalTo: subtitleLabel.bottomAnchor).isActive = true
         
-        indicator.topAnchor.constraint(equalTo: topAnchor, constant: 5).isActive = true
-        indicator.leftAnchor.constraint(equalTo: leftAnchor, constant: 16).isActive = true
-        indicator.widthAnchor.constraint(equalToConstant: 12).isActive = true
+        indicator.topAnchor.constraint(equalTo: topAnchor).isActive = true
+        indicator.leftAnchor.constraint(equalTo: leftAnchor).isActive = true
+        indicator.widthAnchor.constraint(equalToConstant: 24).isActive = true
         indicator.heightAnchor.constraint(equalTo: indicator.widthAnchor).isActive = true
         
         titleLabel.topAnchor.constraint(equalTo: topAnchor).isActive = true
         titleLabel.leftAnchor.constraint(equalTo: indicator.rightAnchor, constant: 12).isActive = true
         titleLabel.rightAnchor.constraint(lessThanOrEqualTo: rightAnchor, constant: -12).isActive = true
         
-        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor).isActive = true
+        subtitleLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 4).isActive = true
         subtitleLabel.leftAnchor.constraint(equalTo: indicator.rightAnchor, constant: 12).isActive = true
         subtitleLabel.rightAnchor.constraint(lessThanOrEqualTo: rightAnchor, constant: -12).isActive = true
     }
     
     func applyTheme() {
-        indicator?.backgroundColor = .theme.ecosia.secondaryText
+        indicator?.tintColor = .theme.ecosia.primaryBrand
         titleLabel?.textColor = .theme.ecosia.primaryText
         subtitleLabel?.textColor = .theme.ecosia.secondaryText
     }
