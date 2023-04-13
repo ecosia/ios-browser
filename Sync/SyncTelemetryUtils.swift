e714fd83dd8ed735fdb296782d84761667a6758f
Dario Carlomagno <dario.carlomagno@gmail.com>
2023-04-13 12:11:15 +0200

    Reverting the changes for removing `Glean` to have less conflicts in the long term. This swift file is not added as compiled sources

--- Sync/SyncTelemetryUtils.swift
+++ Sync/SyncTelemetryUtils.swift
@@ -3,7 +3,7 @@
 // file, You can obtain one at http://mozilla.org/MPL/2.0
 
 import UIKit
-// Ecosia: remove Glean dependency // import Glean
+import Glean
 import Shared
 import Account
 import Storage
