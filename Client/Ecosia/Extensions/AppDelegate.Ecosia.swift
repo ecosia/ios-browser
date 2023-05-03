7d97e81d3ac91b7450c1e1faa81c10acc372c218
Dario Carlomagno <dario.carlomagno@gmail.com>
2023-05-03 14:46:16 +0200

    Fix to select the current environment.
    Previously only `prod` was passed

--- Client/Ecosia/Extensions/AppDelegate.Ecosia.swift
+++ Client/Ecosia/Extensions/AppDelegate.Ecosia.swift
@@ -12,6 +12,7 @@ extension AppDelegate {
         Task {
             do {
                 let env: Environment = AppConstants.BuildChannel == .release ? .production : .staging
+                Environment.current = env
                 try await _ = Unleash.start(env: env)
             } catch {
                 debugPrint(error)
