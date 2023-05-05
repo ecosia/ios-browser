db31d1c2845abc7de1ba3d59bc25eed07ede98d5
Dario Carlomagno <dario.carlomagno@ecosia.org>
2023-05-05 17:48:44 +0200

    MOB-1807 Route traffic to bing ab test (#445)

---
+++ Client/Ecosia/Experiments/UnleashBingExperiment.swift
@@ -0,0 +1,33 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Foundation
+import Core
+
+struct BingSearchExperiment {
+    
+    private init() {}
+    
+    static func incrementCounter() {
+        BingSearchExperimentCounter.increment()
+    }
+    
+    static func getCounterCurrentCount() -> Int {
+        BingSearchExperimentCounter.read()
+    }
+    
+    static func isEnabled() -> Bool {
+        Unleash.isEnabled(.bingSearch)
+    }
+    
+    static func makeBingSearchURLFromURL(_ sourceURL: URL) -> URL? {
+        var urlComponents = URLComponents(url: sourceURL.absoluteURL, resolvingAgainstBaseURL: false)
+        urlComponents?.host = "bing.com"
+        return urlComponents?.url
+    }
+    
+    static func trackAnalytics() {
+        Analytics.shared.userSearchViaBingABTest()
+    }
+}
