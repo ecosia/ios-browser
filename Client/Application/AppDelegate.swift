fba1ff4410361e38880b11c1b04a3afc0947b891
Marcus Kida <marcus.kida@bearologics.com>
2023-06-05 10:23:49 +0200

    MOB-1759: QA - Observe theme changes to change window tintColor

--- Client/Application/AppDelegate.swift
+++ Client/Application/AppDelegate.swift
@@ -193,11 +193,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 extension AppDelegate {
 
     func startListeningForThemeUpdates() {
-        NotificationCenter.default.addObserver(forName: .DisplayThemeChanged, object: nil, queue: .main) { (_) -> Void in
+        NotificationCenter.default.addObserver(forName: .DisplayThemeChanged, object: nil, queue: .main) { [weak self] (_) -> Void in
+            self?.window?.tintColor = .theme.ecosia.primaryBrand
             if !LegacyThemeManager.instance.systemThemeIsOn {
-                self.window?.overrideUserInterfaceStyle = LegacyThemeManager.instance.userInterfaceStyle
+                self?.window?.overrideUserInterfaceStyle = LegacyThemeManager.instance.userInterfaceStyle
             } else {
-                self.window?.overrideUserInterfaceStyle = .unspecified
+                self?.window?.overrideUserInterfaceStyle = .unspecified
             }
         }
     }
