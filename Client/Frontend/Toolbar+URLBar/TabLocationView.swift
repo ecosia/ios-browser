5524476aa67951f6c11a7b60a3d04fd0873fe222
Dario Carlomagno <dario.carlomagno@gmail.com>
2023-06-09 10:24:08 +0200

    Fix after PR review

--- Client/Frontend/Toolbar+URLBar/TabLocationView.swift
+++ Client/Frontend/Toolbar+URLBar/TabLocationView.swift
@@ -67,8 +67,8 @@ class TabLocationView: UIView {
 
     lazy var urlTextField: URLTextField = .build { urlTextField in
         /* Ecosia: removing obsolete implementation as we don't support 4S anymore
-        // Prevent the field from compressing the toolbar buttons on the 4S in landscape.
-        urlTextField.setContentCompressionResistancePriority(UILayoutPriority(rawValue: 250), for: .horizontal)
+         Original reason: Prevent the field from compressing the toolbar buttons on the 4S in landscape.
+         urlTextField.setContentCompressionResistancePriority(UILayoutPriority(rawValue: 250), for: .horizontal)
          */
         urlTextField.accessibilityIdentifier = "url"
         urlTextField.accessibilityActionsSource = self
