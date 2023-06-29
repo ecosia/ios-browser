c169803182b2e9e464767c67cac29f948f87e119
Dario Carlomagno <dario.carlomagno@gmail.com>
2023-06-29 16:56:32 +0200

    enriched the URLRequest in autocomplete with ecosia parameter

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -1143,8 +1143,9 @@ class BrowserViewController: UIViewController {
     func finishEditingAndSubmit(_ url: URL, visitType: VisitType, forTab tab: Tab) {
         urlBar.currentURL = url
         leaveOverlayMode(didCancel: false)
-
-        if let nav = tab.loadRequest(URLRequest(url: url)) {
+        var urlRequest = URLRequest(url: url)
+        urlRequest.addLanguageRegionHeader()
+        if let nav = tab.loadRequest(urlRequest) {
             self.recordNavigationInTab(tab, navigation: nav, visitType: visitType)
         }
     }
