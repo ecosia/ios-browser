33ca24b97f54d5a49b2eb2387ef96fc596950b0d
Dario Carlomagno <dario.carlomagno@ecosia.org>
2023-07-31 19:57:17 +0200

    Update Client/Ecosia/UI/Impact/MultiplyImpact.swift

--- Client/Ecosia/UI/Impact/MultiplyImpact.swift
+++ Client/Ecosia/UI/Impact/MultiplyImpact.swift
@@ -116,7 +116,6 @@ final class MultiplyImpact: UIViewController, NotificationThemeable {
         let content = UIView()
         content.translatesAutoresizingMaskIntoConstraints = false
         scroll.addSubview(content)
-        
         let topBackground = UIView()
         topBackground.translatesAutoresizingMaskIntoConstraints = false
         topBackground.backgroundColor = .theme.ecosia.primaryBrand.withAlphaComponent(0.2)
