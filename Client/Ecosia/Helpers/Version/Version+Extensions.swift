56b31ddd65133ffd3e58ebea5fad381fed7b10a4
Dario Carlomagno <dario.carlomagno@gmail.com>
2023-09-13 15:34:34 +0200

    Updated description for saving and updating

--- Client/Ecosia/Helpers/Version/Version+Extensions.swift
+++ Client/Ecosia/Helpers/Version/Version+Extensions.swift
@@ -6,13 +6,31 @@ import Shared
 
 /// Extension handling previous version retrieval and saving current version.
 extension Version {
-        
+    
+    /// Save the specified app version to `UserDefaults`.
+    ///
+    /// This method takes a `Version` object and saves its description to `UserDefaults`
+    /// using a parametrized key.
+    ///
+    /// - Parameters:
+    ///   - key: The `String` representing the value by which we would like to store our version.
+    ///   - prefs: The `UserDefaults` instance to use for saving the version.
+    ///            Defaults to `UserDefaults.standard`.
     static func saved(forKey key: String, using prefs: UserDefaults = UserDefaults.standard) -> Version? {
         guard let savedKey = prefs.string(forKey: key) else { return nil }
         return Version(savedKey)
     }
     
-    
+    /// Update the specified app version to `UserDefaults`.
+    ///
+    /// This method takes a `String` key to utilize to its `UserDefaults`
+    ///
+    /// - Parameters:
+    ///   - key: The `String` representing the value by which we would like to store our version.
+    ///   - provider: The `AppVersionInfoProvider` serving the `version` `String`.
+    ///               Defaults to `DefaultAppVersionInfoProvider`.
+    ///   - prefs: The `UserDefaults` instance to use for saving the version.
+    ///            Defaults to `UserDefaults.standard`.
     static func updateFromCurrent(forKey key: String,
                                   provider: AppVersionInfoProvider = DefaultAppVersionInfoProvider(),
                                   using prefs: UserDefaults = UserDefaults.standard) {
