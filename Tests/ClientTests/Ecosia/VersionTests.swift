cc17d018ac686123a501791eaaa07f21efbaeb20
Dario Carlomagno <dario.carlomagno@gmail.com>
2023-09-13 15:28:02 +0200

    Fixing last bit of `VersionTests`

--- Tests/ClientTests/Ecosia/VersionTests.swift
+++ Tests/ClientTests/Ecosia/VersionTests.swift
@@ -9,10 +9,10 @@ final class VersionTests: XCTestCase {
     
     private static let appVersionUpdateTestKey = "appVersionUpdateTestKey"
     
-    override class func setUp() {
+    override func setUp() async throws {
         UserDefaults.standard.removeObject(forKey: Self.appVersionUpdateTestKey)
     }
-    
+        
     // Test initialization
     func testInitialization() {
         XCTAssertNotNil(Version("1.0.0"))
@@ -47,39 +47,49 @@ final class VersionTests: XCTestCase {
     
     // Test Version retrieval and saving using UserDefaults
     func testVersionStorage() {
-        let version1 = Version("1.0.0")
-        let version2 = Version("1.0.1")
+        let version1 = Version("1.0.0")!
+        let version2 = Version("1.0.1")!
 
-        Version.updateFromCurrent(forKey: Self.appVersionUpdateTestKey)
+        Version.updateFromCurrent(forKey: Self.appVersionUpdateTestKey,
+                                  provider: MockAppVersionInfoProvider(mockedAppVersion: version1.description))
         
-        XCTAssertEqual(Version.saved(forKey: Self.appVersionUpdateTestKey)?.description, version1?.description)
+        XCTAssertEqual(Version.saved(forKey: Self.appVersionUpdateTestKey)?.description, version1.description)
 
-        Version.updateFromCurrent(forKey: Self.appVersionUpdateTestKey, provider: MockAppVersionInfoProvider(appVersion: "1.0.1"))
+        Version.updateFromCurrent(forKey: Self.appVersionUpdateTestKey, provider: MockAppVersionInfoProvider(mockedAppVersion: "1.0.1"))
         
-        XCTAssertEqual(Version.saved(forKey: Self.appVersionUpdateTestKey)?.description, version2?.description)
+        XCTAssertEqual(Version.saved(forKey: Self.appVersionUpdateTestKey)?.description, version2.description)
     }
     
     func testDoubleDigitVersions() {
-        let version1 = Version("10.9.8")
-        let version2 = Version("10.10.8")
-        let version3 = Version("11.9.8")
-        let version4 = Version("11.11.11")
+        let version1 = Version("10.9.8")!
+        let version2 = Version("10.10.8")!
+        let version3 = Version("11.9.8")!
+        let version4 = Version("11.11.11")!
 
-        Version.updateFromCurrent(forKey: Self.appVersionUpdateTestKey, provider: MockAppVersionInfoProvider(appVersion: "10.9.8"))
-        XCTAssertEqual(Version.saved(forKey: Self.appVersionUpdateTestKey)?.description, version1?.description)
+        Version.updateFromCurrent(forKey: Self.appVersionUpdateTestKey, provider: MockAppVersionInfoProvider(mockedAppVersion: version1.description))
+        XCTAssertEqual(Version.saved(forKey: Self.appVersionUpdateTestKey)?.description, version1.description)
 
-        Version.updateFromCurrent(forKey: Self.appVersionUpdateTestKey, provider: MockAppVersionInfoProvider(appVersion: "10.10.8"))
-        XCTAssertEqual(Version.saved(forKey: Self.appVersionUpdateTestKey)?.description, version2?.description)
+        Version.updateFromCurrent(forKey: Self.appVersionUpdateTestKey, provider: MockAppVersionInfoProvider(mockedAppVersion: version2.description))
+        XCTAssertEqual(Version.saved(forKey: Self.appVersionUpdateTestKey)?.description, version2.description)
 
-        XCTAssertTrue(version2! < version3!)
-        XCTAssertTrue(version3! < version4!)
-        XCTAssertFalse(version3! == version4!)
+        XCTAssertTrue(version2 < version3)
+        XCTAssertTrue(version3 < version4)
+        XCTAssertFalse(version3 == version4)
     }
 }
 
 extension VersionTests {
     
     struct MockAppVersionInfoProvider: AppVersionInfoProvider {
-        var appVersion: String = "1.0.0"
+        
+        var mockedAppVersion: String
+        
+        init(mockedAppVersion: String) {
+            self.mockedAppVersion = mockedAppVersion
+        }
+        
+        var version: String {
+            mockedAppVersion
+        }
     }
 }
