aa7cf1c1fc6c19b52f7ff406a08e79361ea3018b
Dario Carlomagno <dario.carlomagno@gmail.com>
2023-09-25 14:18:59 +0200

    Invert check to improve performance

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -163,7 +163,7 @@ class BrowserViewController: UIViewController {
     }
 
     fileprivate var shouldShowDefaultBrowserPromo: Bool { profile.prefs.intForKey(PrefsKeys.IntroSeen) == nil }
-    fileprivate var shouldShowWhatsNewPageScreen: Bool { whatsNewDataProvider.shouldShowWhatsNewPage && !User.shared.firstTime }
+    fileprivate var shouldShowWhatsNewPageScreen: Bool { !User.shared.firstTime && whatsNewDataProvider.shouldShowWhatsNewPage }
     
     let whatsNewDataProvider = WhatsNewLocalDataProvider()
     
