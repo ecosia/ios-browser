8501fe38c5390dc483a353f6dffefdf77e142516
Dario Carlomagno <dario.carlomagno@gmail.com>
2023-11-28 17:25:55 +0100

    Search Bar stays on top by default

--- Client/Nimbus/NimbusSearchBarLayer.swift
+++ Client/Nimbus/NimbusSearchBarLayer.swift
@@ -7,11 +7,11 @@ import Foundation
 final class NimbusSearchBarLayer {
     // MARK: - Public methods
     public func getDefaultPosition(from nimbus: FxNimbus = FxNimbus.shared) -> SearchBarPosition {
-        /* Ecosia: Nimbus
+        /* Ecosia: Search Bar stays on top by default
         let isAtBottom = nimbus.features.search.value().awesomeBar.position.isBottom
 
         return isAtBottom ? .bottom : .top
          */
-        .bottom
+        .top
     }
 }
