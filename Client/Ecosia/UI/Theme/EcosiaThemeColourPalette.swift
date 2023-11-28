084296604dec65472314da4075bb7bf4731cd932
Dario Carlomagno <dario.carlomagno@gmail.com>
2023-11-28 17:08:54 +0100

    Review `EcosiaThemeColourPalette`

--- Client/Ecosia/UI/Theme/EcosiaThemeColourPalette.swift
+++ Client/Ecosia/UI/Theme/EcosiaThemeColourPalette.swift
@@ -2,11 +2,19 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
 
-import Common
+/*
+ 
+The purpose of this file is to build an adapter layer and begin to utilize the `ThemeColourPalette` that Firefox provide.
+WHY?
+In the previous releases of Firefox the theming architecture relied on a protocol approach without any sort of dependency injection as part of the `applyTheme()` function.
+ However, since the codebase bigger restructure, the Theming has gone thru a major refactor as well.
+ By having this adapter in, we can benefit of the `Theme` object being passed as part of the `func applyTheme(theme: Theme)` function so that all the places having it implemented will receive the new colors.
+ The setup of these Dark and Light Ecosia's Colour Palette is definted by the `EcosiaThemeManager`.
+ However, the need of a `fallbackDefaultThemeManager` of type `DefaultThemeManager` is crucial as we don't have all the colors defined ourselves and we rely on the Firefox ones we can't get access to as part of the `BrowserKit` package.
+ Once and if we'll have all the colors defined, we can remove the `fallbackDefaultThemeManager` variable.
+*/
 
-fileprivate var themeManager: ThemeManager {
-    AppContainer.shared.resolve()
-}
+import Common
 
 public struct EcosiaLightTheme: Theme {
     public var type: ThemeType = .light
@@ -25,84 +33,88 @@ public struct EcosiaDarkTheme: Theme {
 private class EcosiaDarkColourPalette: EcosiaLightColourPalette {}
 
 private class EcosiaLightColourPalette: ThemeColourPalette {
+    
+    private static var fallbackDefaultThemeManager: ThemeManager = {
+        DefaultThemeManager(sharedContainerIdentifier: AppInfo.sharedContainerIdentifier)
+    }()
         
     // MARK: - Layers
-    var layer1: UIColor = themeManager.currentTheme.colors.layer1
-    var layer2: UIColor = themeManager.currentTheme.colors.layer2
-    var layer3: UIColor = themeManager.currentTheme.colors.layer3
-    var layer4: UIColor = themeManager.currentTheme.colors.layer4
-    var layer5: UIColor = themeManager.currentTheme.colors.layer5
-    var layer6: UIColor = themeManager.currentTheme.colors.layer6
-    var layer5Hover: UIColor = themeManager.currentTheme.colors.layer5Hover
-    var layerScrim: UIColor = themeManager.currentTheme.colors.layerScrim
-    var layerGradient = themeManager.currentTheme.colors.layerGradient
-    var layerGradientOverlay = themeManager.currentTheme.colors.layerGradientOverlay
-    var layerAccentNonOpaque: UIColor = themeManager.currentTheme.colors.layerAccentNonOpaque
-    var layerAccentPrivate: UIColor = themeManager.currentTheme.colors.layerAccentPrivate
-    var layerAccentPrivateNonOpaque: UIColor = themeManager.currentTheme.colors.layerAccentPrivateNonOpaque
-    var layerLightGrey30: UIColor = themeManager.currentTheme.colors.layerLightGrey30
-    var layerSepia: UIColor = themeManager.currentTheme.colors.layerSepia
-    var layerInfo: UIColor = themeManager.currentTheme.colors.layerInfo
-    var layerConfirmation: UIColor = themeManager.currentTheme.colors.layerConfirmation
+    var layer1: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layer1
+    var layer2: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layer2
+    var layer3: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layer3
+    var layer4: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layer4
+    var layer5: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layer5
+    var layer6: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layer6
+    var layer5Hover: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layer5Hover
+    var layerScrim: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerScrim
+    var layerGradient = fallbackDefaultThemeManager.currentTheme.colors.layerGradient
+    var layerGradientOverlay = fallbackDefaultThemeManager.currentTheme.colors.layerGradientOverlay
+    var layerAccentNonOpaque: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerAccentNonOpaque
+    var layerAccentPrivate: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerAccentPrivate
+    var layerAccentPrivateNonOpaque: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerAccentPrivateNonOpaque
+    var layerLightGrey30: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerLightGrey30
+    var layerSepia: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerSepia
+    var layerInfo: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerInfo
+    var layerConfirmation: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerConfirmation
     var layerWarning: UIColor = UIColor.legacyTheme.ecosia.warning
-    var layerError: UIColor = themeManager.currentTheme.colors.layerError
-    var layerRatingA: UIColor = themeManager.currentTheme.colors.layerRatingA
-    var layerRatingASubdued: UIColor = themeManager.currentTheme.colors.layerRatingASubdued
-    var layerRatingB: UIColor = themeManager.currentTheme.colors.layerRatingB
-    var layerRatingBSubdued: UIColor = themeManager.currentTheme.colors.layerRatingBSubdued
-    var layerRatingC: UIColor = themeManager.currentTheme.colors.layerRatingC
-    var layerRatingCSubdued: UIColor = themeManager.currentTheme.colors.layerRatingCSubdued
-    var layerRatingD: UIColor = themeManager.currentTheme.colors.layerRatingD
-    var layerRatingDSubdued: UIColor = themeManager.currentTheme.colors.layerRatingDSubdued
-    var layerRatingF: UIColor = themeManager.currentTheme.colors.layerRatingF
-    var layerRatingFSubdued: UIColor = themeManager.currentTheme.colors.layerRatingFSubdued
+    var layerError: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerError
+    var layerRatingA: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerRatingA
+    var layerRatingASubdued: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerRatingASubdued
+    var layerRatingB: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerRatingB
+    var layerRatingBSubdued: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerRatingBSubdued
+    var layerRatingC: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerRatingC
+    var layerRatingCSubdued: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerRatingCSubdued
+    var layerRatingD: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerRatingD
+    var layerRatingDSubdued: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerRatingDSubdued
+    var layerRatingF: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerRatingF
+    var layerRatingFSubdued: UIColor = fallbackDefaultThemeManager.currentTheme.colors.layerRatingFSubdued
 
     // MARK: - Actions
     var actionPrimary: UIColor = UIColor.legacyTheme.ecosia.primaryButton
     var actionPrimaryHover: UIColor = UIColor.legacyTheme.ecosia.primaryButtonActive
     var actionSecondary: UIColor = UIColor.legacyTheme.ecosia.secondaryButton
-    var actionSecondaryHover: UIColor = themeManager.currentTheme.colors.actionSecondaryHover
-    var formSurfaceOff: UIColor = themeManager.currentTheme.colors.formSurfaceOff
-    var formKnob: UIColor = themeManager.currentTheme.colors.formKnob
-    var indicatorActive: UIColor = themeManager.currentTheme.colors.indicatorActive
-    var indicatorInactive: UIColor = themeManager.currentTheme.colors.indicatorInactive
-    var actionConfirmation: UIColor = themeManager.currentTheme.colors.actionConfirmation
+    var actionSecondaryHover: UIColor = fallbackDefaultThemeManager.currentTheme.colors.actionSecondaryHover
+    var formSurfaceOff: UIColor = fallbackDefaultThemeManager.currentTheme.colors.formSurfaceOff
+    var formKnob: UIColor = fallbackDefaultThemeManager.currentTheme.colors.formKnob
+    var indicatorActive: UIColor = fallbackDefaultThemeManager.currentTheme.colors.indicatorActive
+    var indicatorInactive: UIColor = fallbackDefaultThemeManager.currentTheme.colors.indicatorInactive
+    var actionConfirmation: UIColor = fallbackDefaultThemeManager.currentTheme.colors.actionConfirmation
     var actionWarning: UIColor = UIColor.legacyTheme.ecosia.warning
-    var actionError: UIColor = themeManager.currentTheme.colors.actionError
+    var actionError: UIColor = fallbackDefaultThemeManager.currentTheme.colors.actionError
 
     // MARK: - Text
     var textPrimary: UIColor = UIColor.legacyTheme.ecosia.primaryText
     var textSecondary: UIColor = UIColor.legacyTheme.ecosia.secondaryText
-    var textSecondaryAction: UIColor = themeManager.currentTheme.colors.textSecondaryAction
-    var textDisabled: UIColor = themeManager.currentTheme.colors.textDisabled
-    var textWarning: UIColor = themeManager.currentTheme.colors.textWarning
-    var textAccent: UIColor = themeManager.currentTheme.colors.textAccent
-    var textOnDark: UIColor = themeManager.currentTheme.colors.textOnDark
-    var textOnLight: UIColor = themeManager.currentTheme.colors.textOnLight
+    var textSecondaryAction: UIColor = fallbackDefaultThemeManager.currentTheme.colors.textSecondaryAction
+    var textDisabled: UIColor = fallbackDefaultThemeManager.currentTheme.colors.textDisabled
+    var textWarning: UIColor = fallbackDefaultThemeManager.currentTheme.colors.textWarning
+    var textAccent: UIColor = UIColor.legacyTheme.ecosia.primaryButton
+    var textOnDark: UIColor = fallbackDefaultThemeManager.currentTheme.colors.textOnDark
+    var textOnLight: UIColor = fallbackDefaultThemeManager.currentTheme.colors.textOnLight
     var textInverted: UIColor = UIColor.legacyTheme.ecosia.primaryTextInverted
 
     // MARK: - Icons
     var iconPrimary: UIColor = UIColor.legacyTheme.ecosia.primaryIcon
     var iconSecondary: UIColor = UIColor.legacyTheme.ecosia.secondaryIcon
-    var iconDisabled: UIColor = themeManager.currentTheme.colors.iconDisabled
-    var iconAction: UIColor = themeManager.currentTheme.colors.iconAction
-    var iconOnColor: UIColor = themeManager.currentTheme.colors.iconOnColor
-    var iconWarning: UIColor = themeManager.currentTheme.colors.iconWarning
-    var iconSpinner: UIColor = themeManager.currentTheme.colors.iconSpinner
-    var iconAccentViolet: UIColor = themeManager.currentTheme.colors.iconAccentViolet
-    var iconAccentBlue: UIColor = themeManager.currentTheme.colors.iconAccentBlue
-    var iconAccentPink: UIColor = themeManager.currentTheme.colors.iconAccentPink
-    var iconAccentGreen: UIColor = themeManager.currentTheme.colors.iconAccentGreen
-    var iconAccentYellow: UIColor = themeManager.currentTheme.colors.iconAccentYellow
+    var iconDisabled: UIColor = fallbackDefaultThemeManager.currentTheme.colors.iconDisabled
+    var iconAction: UIColor = fallbackDefaultThemeManager.currentTheme.colors.iconAction
+    var iconOnColor: UIColor = fallbackDefaultThemeManager.currentTheme.colors.iconOnColor
+    var iconWarning: UIColor = fallbackDefaultThemeManager.currentTheme.colors.iconWarning
+    var iconSpinner: UIColor = fallbackDefaultThemeManager.currentTheme.colors.iconSpinner
+    var iconAccentViolet: UIColor = fallbackDefaultThemeManager.currentTheme.colors.iconAccentViolet
+    var iconAccentBlue: UIColor = fallbackDefaultThemeManager.currentTheme.colors.iconAccentBlue
+    var iconAccentPink: UIColor = fallbackDefaultThemeManager.currentTheme.colors.iconAccentPink
+    var iconAccentGreen: UIColor = fallbackDefaultThemeManager.currentTheme.colors.iconAccentGreen
+    var iconAccentYellow: UIColor = fallbackDefaultThemeManager.currentTheme.colors.iconAccentYellow
 
     // MARK: - Border
-    var borderPrimary: UIColor = themeManager.currentTheme.colors.borderPrimary
-    var borderAccent: UIColor = themeManager.currentTheme.colors.borderAccent
-    var borderAccentNonOpaque: UIColor = themeManager.currentTheme.colors.borderAccentNonOpaque
-    var borderAccentPrivate: UIColor = themeManager.currentTheme.colors.borderAccentPrivate
-    var borderInverted: UIColor = themeManager.currentTheme.colors.borderInverted
+    var borderPrimary: UIColor = fallbackDefaultThemeManager.currentTheme.colors.borderPrimary
+    var borderAccent: UIColor = fallbackDefaultThemeManager.currentTheme.colors.borderAccent
+    var borderAccentNonOpaque: UIColor = fallbackDefaultThemeManager.currentTheme.colors.borderAccentNonOpaque
+    var borderAccentPrivate: UIColor = fallbackDefaultThemeManager.currentTheme.colors.borderAccentPrivate
+    var borderInverted: UIColor = fallbackDefaultThemeManager.currentTheme.colors.borderInverted
 
     // MARK: - Shadow
-    var shadowDefault: UIColor = themeManager.currentTheme.colors.shadowDefault
+    var shadowDefault: UIColor = fallbackDefaultThemeManager.currentTheme.colors.shadowDefault
 }
 
