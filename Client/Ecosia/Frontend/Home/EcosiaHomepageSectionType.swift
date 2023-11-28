5283fdca822c5e3ff96a4dd8867b9e42ad8c785d
Dario Carlomagno <dario.carlomagno@gmail.com>
2023-11-28 15:14:03 +0100

    Add Ecosia comment to `HomePageSectionType`

--- Client/Ecosia/Frontend/Home/EcosiaHomepageSectionType.swift
+++ Client/Ecosia/Frontend/Home/EcosiaHomepageSectionType.swift
@@ -1,3 +1,8 @@
+/*
+ Ecosia: This file replaces the one contained in Client/Frontend/Home
+ It is done so that we will have minimum conflicts when major updates are needed
+ */
+
 // This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at http://mozilla.org/MPL/2.0/
