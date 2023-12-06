af561a590acbab7eb1d3c6a3fa7c36289809c8fe
Dario Carlomagno <dario.carlomagno@gmail.com>
2023-12-06 18:57:38 +0100

    Implement APNConsent
    Add necessary assets and strings as well

---
+++ Client/Ecosia/UI/APNConsent/UnleashAPNConsentViewModel.swift
@@ -0,0 +1,53 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Core
+
+final class UnleashAPNConsentViewModel: APNConsentViewModelProtocol {
+    
+    var title: String {
+        titleMatchingVariant
+    }
+    
+    var image: UIImage? {
+        imageMatchinVariant
+    }
+        
+    var listItems: [APNConsentListItem] {
+        listItemsMatchingVariant
+    }
+}
+
+extension UnleashAPNConsentViewModel {
+    
+    private static let listItemsVariantNameControl: [APNConsentListItem] = [
+        APNConsentListItem(title: .localized(.apnConsentVariantNameControlFirstItemTitle)),
+        APNConsentListItem(title: .localized(.apnConsentVariantNameControlSecondItemTitle))
+    ]
+    private static let listItemsVariantNameTest1: [APNConsentListItem] = [
+        APNConsentListItem(title: .localized(.apnConsentVariantNameTest1FirstItemTitle)),
+        APNConsentListItem(title: .localized(.apnConsentVariantNameTest1SecondItemTitle))
+    ]
+    
+    private var listItemsMatchingVariant: [APNConsentListItem] {
+        switch EngagementServiceExperiment.variantName {
+        case "test1": return Self.listItemsVariantNameTest1
+        default: return Self.listItemsVariantNameControl
+        }
+    }
+    
+    private var titleMatchingVariant: String {
+        switch EngagementServiceExperiment.variantName {
+        case "test1": return .localized(.apnConsentVariantNameTest1CTAAllowButtonTitle)
+        default: return .localized(.apnConsentVariantNameControlCTAAllowButtonTitle)
+        }
+    }
+    
+    private var imageMatchinVariant: UIImage? {
+        switch EngagementServiceExperiment.variantName {
+        case "test1": return .init(named: "apnConsentImageTest1")
+        default: return .init(named: "apnConsentImageControl")
+        }
+    }
+}
