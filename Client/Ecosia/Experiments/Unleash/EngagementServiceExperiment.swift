5871d8828fe37de8eabdbe287ab0e93b420b8742
Dario Carlomagno <dario.carlomagno@gmail.com>
2023-12-07 14:41:01 +0100

    Update EngagementServiceExperiment with the correct Unleash key

--- Client/Ecosia/Experiments/Unleash/EngagementServiceExperiment.swift
+++ Client/Ecosia/Experiments/Unleash/EngagementServiceExperiment.swift
@@ -12,16 +12,16 @@ struct EngagementServiceExperiment {
     private init() {}
 
     static var isEnabled: Bool {
-        Unleash.isEnabled(.defaultBrowser)
+        Unleash.isEnabled(.brazeEngagementService)
     }
 
     static func minSearches() -> Int {
-        let variant = Unleash.getVariant(.defaultBrowser)
+        let variant = Unleash.getVariant(.brazeEngagementService)
         return minSearches(for: variant)
     }
     
     static var variantName: String {
-        return Unleash.getVariant(.defaultBrowser).name
+        return Unleash.getVariant(.brazeEngagementService).name
     }
 
     private static func minSearches(for variant: Unleash.Variant) -> Int {
