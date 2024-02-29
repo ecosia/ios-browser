ecd14e809aad8e9946fc8680c2ac9c4e6b2a27e3
Dario Carlomagno <dario.carlomagno@ecosia.org>
2024-02-29 18:26:16 +0100

    [MOB-2258] Update HomeViewModel test according to current items

--- EcosiaTests/EcosiaHomeViewModelTests.swift
+++ EcosiaTests/EcosiaHomeViewModelTests.swift
@@ -30,7 +30,7 @@ class EcosiaHomeViewModelTests: XCTestCase {
 
     // MARK: Number of sections
 
-    func testNumberOfSection_withoutUpdatingData_has6Sections() {
+    func testNumberOfSection_withoutUpdatingData_has5Sections() {
         // Ecosia: Update Viewmodel
         let viewModel = HomepageViewModel(profile: profile,
                                           isPrivate: false, 
@@ -38,15 +38,13 @@ class EcosiaHomeViewModelTests: XCTestCase {
                                           referrals: .init(),
                                           theme: EcosiaLightTheme())
         // Ecosia: Update shown sections
-        XCTAssertEqual(viewModel.shownSections.count, 6)
+        XCTAssertEqual(viewModel.shownSections.count, 5)
         XCTAssertEqual(viewModel.shownSections[0], HomepageSectionType.logoHeader)
-        // Bookmark Nudge depends on User.showsBookmarksNTPNudgeCard()
-        XCTAssertEqual(viewModel.shownSections[1], HomepageSectionType.bookmarkNudge)
-        XCTAssertEqual(viewModel.shownSections[2], HomepageSectionType.libraryShortcuts)
-        XCTAssertEqual(viewModel.shownSections[3], HomepageSectionType.impact)
+        XCTAssertEqual(viewModel.shownSections[1], HomepageSectionType.libraryShortcuts)
+        XCTAssertEqual(viewModel.shownSections[2], HomepageSectionType.impact)
         // News is not shown without items
         // XCTAssertEqual(viewModel.shownSections[4], HomepageSectionType.news)
-        XCTAssertEqual(viewModel.shownSections[4], HomepageSectionType.aboutEcosia)
-        XCTAssertEqual(viewModel.shownSections[5], HomepageSectionType.ntpCustomization)
+        XCTAssertEqual(viewModel.shownSections[3], HomepageSectionType.aboutEcosia)
+        XCTAssertEqual(viewModel.shownSections[4], HomepageSectionType.ntpCustomization)
     }
 }
