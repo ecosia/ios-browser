c49d6cde8a57f9085030c9c80b1f88ee9ec88b93
Dario Carlomagno <dario.carlomagno@ecosia.org>
2024-07-09 15:07:44 +0200

    Update `LaunchCoordinatorTests` await config

--- firefox-ios/firefox-ios-tests/Tests/ClientTests/Coordinators/Launch/LaunchCoordinatorTests.swift
+++ firefox-ios/firefox-ios-tests/Tests/ClientTests/Coordinators/Launch/LaunchCoordinatorTests.swift
@@ -41,7 +41,8 @@ final class LaunchCoordinatorTests: XCTestCase {
         let introScreenManager = IntroScreenManager(prefs: profile.prefs)
         let subject = createSubject(isIphone: false)
         subject.start(with: .intro(manager: introScreenManager))
-
+        // Ecosia: Remove wait when the `SkipOnboardingExperiment` finishes
+        wait(0.5)
         XCTAssertEqual(mockRouter.presentCalled, 1)
         XCTAssertEqual(mockRouter.setRootViewControllerCalled, 0)
         let presentedViewController = try XCTUnwrap(mockRouter.presentedViewController)
@@ -54,7 +55,8 @@ final class LaunchCoordinatorTests: XCTestCase {
         let introScreenManager = IntroScreenManager(prefs: profile.prefs)
         let subject = createSubject(isIphone: true)
         subject.start(with: .intro(manager: introScreenManager))
-
+        // Ecosia: Remove wait when the `SkipOnboardingExperiment` finishes
+        wait(0.5)
         XCTAssertEqual(mockRouter.presentCalled, 1)
         XCTAssertEqual(mockRouter.setRootViewControllerCalled, 0)
         let pushedVC = try XCTUnwrap(mockRouter.presentedViewController)
