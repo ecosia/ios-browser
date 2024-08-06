e754bf4d8555f42eb24dc948ff90b89e9991ae70
Dario Carlomagno <dario.carlomagno@ecosia.org>
2024-08-06 20:33:08 +0200

    Refactor NTP tests

--- EcosiaTests/SnapshotTests/NTPTests.swift
+++ EcosiaTests/SnapshotTests/NTPTests.swift
@@ -10,58 +10,54 @@ import Core
 import MozillaAppServices
 @testable import Client
 
-class HomepageViewControllerTests: XCTestCase {
+final class HomepageViewControllerTests: XCTestCase {
     
     var profile: MockProfile!
-
+    
     override func setUp() {
         super.setUp()
         DependencyHelperMock().bootstrapDependencies()
         profile = MockProfile()
         LegacyFeatureFlagsManager.shared.initializeDeveloperFeatures(with: profile)
     }
-
+    
     override func tearDown() {
         super.tearDown()
         AppContainer.shared.reset()
         profile = nil
     }
-
+    
     func testNTPShowingImpactIntro() {
         User.shared.showImpactIntro()
-        let tabManager = TabManagerImplementation(profile: profile, imageStore: nil)
-        let urlBar = URLBarView(profile: profile)
-        let overlayManager = MockOverlayModeManager()
-        overlayManager.setURLBar(urlBarView: urlBar)
-
-        let homePageViewController = HomepageViewController(profile: profile,
-                                                               toastContainer: UIView(),
-                                                               tabManager: tabManager,
-                                                               overlayManager: overlayManager,
-                                                               referrals: .init(),
-                                                               delegate: nil)
-        
-        // Providing precision = 0.95 to accommodate the trees and the money counter updates which will result into different numbers
-        // for different snapshots.
-        SnapshotTestHelper.assertSnapshot(of: homePageViewController, wait: 3.0, precision: 0.95, testName: "NTP_with_impact_intro")
+        snapshotNTP(impactIntroShown: true)
     }
     
     func testNTPImpactIntroHidden() {
         User.shared.hideImpactIntro()
+        snapshotNTP(impactIntroShown: false)
+    }
+}
+
+extension HomepageViewControllerTests {
+    fileprivate func snapshotNTP(impactIntroShown: Bool) {
         let tabManager = TabManagerImplementation(profile: profile, imageStore: nil)
         let urlBar = URLBarView(profile: profile)
         let overlayManager = MockOverlayModeManager()
         overlayManager.setURLBar(urlBarView: urlBar)
-
+        
         let homePageViewController = HomepageViewController(profile: profile,
-                                                               toastContainer: UIView(),
-                                                               tabManager: tabManager,
-                                                               overlayManager: overlayManager,
-                                                               referrals: .init(),
-                                                               delegate: nil)
+                                                            toastContainer: UIView(),
+                                                            tabManager: tabManager,
+                                                            overlayManager: overlayManager,
+                                                            referrals: .init(),
+                                                            delegate: nil)
         
         // Providing precision = 0.95 to accommodate the trees and the money counter updates which will result into different numbers
         // for different snapshots.
-        SnapshotTestHelper.assertSnapshot(of: homePageViewController, wait: 3.0, precision: 0.95, testName: "NTP_without_impact_intro")
+        let snapshotTitle = impactIntroShown ? "NTP_with_impact_intro" : "NTP_without_impact_intro"
+        SnapshotTestHelper.assertSnapshot(of: homePageViewController,
+                                          wait: 4.0,
+                                          precision: 0.95,
+                                          testName: snapshotTitle)
     }
 }
