bdcf390bbf781c760953097156edd4a250f9576e
Dario Carlomagno <dario.carlomagno@ecosia.org>
2024-08-08 02:01:58 +0200

    Remove NTPTests due to favicon fetching on main thread issue

--- EcosiaTests/SnapshotTests/NTP/NTPTests.swift
+++ EcosiaTests/SnapshotTests/NTP/NTPTests.swift
@@ -8,36 +8,36 @@ import Core
 import Common
 @testable import Client
 
-//final class HomepageViewControllerTests: SnapshotBaseTests {
-//    
-//    func testNTPShowingImpactIntro() {
-//        User.shared.showImpactIntro()
-//        snapshotNTP(impactIntroShown: true)
-//    }
-//    
-//    func testNTPImpactIntroHidden() {
-//        User.shared.hideImpactIntro()
-//        snapshotNTP(impactIntroShown: false)
-//    }
-//}
-//
-//extension HomepageViewControllerTests {
-//    fileprivate func snapshotNTP(impactIntroShown: Bool) {
-//        SnapshotTestHelper.assertSnapshot(initializingWith: {
-//            let tabManager = TabManagerImplementation(profile: self.profile, imageStore: nil)
-//            let urlBar = URLBarView(profile: self.profile)
-//            let overlayManager = MockOverlayModeManager()
-//            overlayManager.setURLBar(urlBarView: urlBar)
-//            
-//            let homePageViewController = HomepageViewController(profile: self.profile,
-//                                                                toastContainer: UIView(),
-//                                                                tabManager: tabManager,
-//                                                                overlayManager: overlayManager,
-//                                                                referrals: .init(),
-//                                                                delegate: nil)
-//            return homePageViewController
-//        },
-//        precision: 0.95,
-//        testName: impactIntroShown ? "NTP_with_impact_intro" : "NTP_without_impact_intro")
-//    }
-//}
+final class NTPTests: SnapshotBaseTests {
+    
+    func testNTPShowingImpactIntro() {
+        User.shared.showImpactIntro()
+        snapshotNTP(impactIntroShown: true)
+    }
+    
+    func testNTPImpactIntroHidden() {
+        User.shared.hideImpactIntro()
+        snapshotNTP(impactIntroShown: false)
+    }
+}
+
+extension NTPTests {
+    fileprivate func snapshotNTP(impactIntroShown: Bool) {
+        SnapshotTestHelper.assertSnapshot(initializingWith: {
+            let tabManager = TabManagerImplementation(profile: self.profile, imageStore: nil)
+            let urlBar = URLBarView(profile: self.profile)
+            let overlayManager = MockOverlayModeManager()
+            overlayManager.setURLBar(urlBarView: urlBar)
+            
+            let homePageViewController = HomepageViewController(profile: self.profile,
+                                                                toastContainer: UIView(),
+                                                                tabManager: tabManager,
+                                                                overlayManager: overlayManager,
+                                                                referrals: .init(),
+                                                                delegate: nil)
+            return homePageViewController
+        },
+        precision: 0.95,
+        testName: impactIntroShown ? "NTP_with_impact_intro" : "NTP_without_impact_intro")
+    }
+}
