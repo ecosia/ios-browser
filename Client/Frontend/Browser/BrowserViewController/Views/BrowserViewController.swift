7c8f0591d20865b58e3906fa125161258f8eb738
Dario Carlomagno <dario.carlomagno@ecosia.org>
2024-08-28 17:30:03 +0200

    URL Bar doesn't overlay on new tab opened

--- Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController/Views/BrowserViewController.swift
@@ -2560,9 +2560,9 @@ extension BrowserViewController {
         user.referrals.pendingClaim != nil
     }
 
-    func presentInsightfulSheetsIfNeeded() -> Bool {
+    func presentInsightfulSheetsIfNeeded() {
         guard isHomePage(),
-              !showLoadingScreen(for: .shared) else { return false }
+              !showLoadingScreen(for: .shared) else { return }
         
         // TODO: To review this logic as part of the upgrade
         /*
@@ -2579,7 +2579,7 @@ extension BrowserViewController {
             presentAPNConsentIfNeeded
         ]
 
-        return (presentationFunctions.first(where: { $0() }) != nil)
+        _ = presentationFunctions.first(where: { $0() })
     }
 
     private func isHomePage() -> Bool {
