d74b3d991f5a75470040fb80db9d9335158c8501
Dario Carlomagno <dario.carlomagno@ecosia.org>
2024-09-18 11:13:37 +0200

    Add test and move the Install Type check as part of the `isFirstInstall`

--- Client/Ecosia/Analytics/Analytics+Configuration.swift
+++ Client/Ecosia/Analytics/Analytics+Configuration.swift
@@ -57,7 +57,7 @@ extension Analytics {
         return plugin.filter(schemas: [
             Self.installSchema
         ]) { _ in
-            return Self.isFirstInstall(for: identifier) && EcosiaInstallType.get() != .upgrade
+            return Self.isFirstInstall(for: identifier)
         }
     }
 }
@@ -73,7 +73,7 @@ extension Analytics {
             defaults.set(false, forKey: identifier)
             return true
         }()
-        return isFirstTime
+        return isFirstTime && EcosiaInstallType.get() != .upgrade
     }
 }
 
