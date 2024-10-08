36b1ea1a254bcb739f689575fecd701998011cd3
Dario Carlomagno <dario.carlomagno@ecosia.org>
2024-10-08 18:38:32 +0200

    Add Sparkles to the SeedCounterView context

--- Client/Ecosia/UI/NTP/ClimateImpactCounter/SeedCounterView.swift
+++ Client/Ecosia/UI/NTP/ClimateImpactCounter/SeedCounterView.swift
@@ -10,9 +10,11 @@ struct SeedCounterView: View {
     // MARK: - Properties
     
     private let progressManagerType: SeedProgressManagerProtocol.Type
+    private let twinkleDuration = 10.0
     @State private var seedsCollected: Int = 0
     @State private var level: Int = 1
     @State private var progressValue: CGFloat = 0.0
+    @State private var showTwinkle = false
     @StateObject var theme = ArcTheme()
     @Environment(\.themeType) var themeVal
     
@@ -28,13 +30,30 @@ struct SeedCounterView: View {
     // MARK: - View
     
     var body: some View {
-        VStack(spacing: 0) {
-            SeedProgressView(progressValue: progressValue,
-                             theme: theme)
+        ZStack {
+            VStack(spacing: 0) {
+                SeedProgressView(progressValue: progressValue,
+                                 theme: theme)
+                
+                Text("\(Int(seedsCollected))")
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+            }
+            .zIndex(0)
             
-            Text("\(Int(seedsCollected))")
-                .font(.subheadline)
-                .fontWeight(.semibold)
+            if showTwinkle {
+                TwinkleView(active: true)
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .clipped(antialiased: false)
+                    .allowsHitTesting(false)
+                    .zIndex(1)
+                    .onAppear {
+                        // Automatically stop showing twinkle after a delay
+                        DispatchQueue.main.asyncAfter(deadline: .now() + twinkleDuration) {
+                            showTwinkle = false
+                        }
+                    }
+            }
         }
         .onAppear {
             // Add observer for progress updates
@@ -44,10 +63,15 @@ struct SeedCounterView: View {
                 self.level = progressManagerType.loadCurrentLevel()
                 self.progressValue = progressManagerType.calculateInnerProgress()
             }
+            NotificationCenter.default.addObserver(forName: UserDefaultsSeedProgressManager.levelUpNotification, object: nil, queue: .main) { _ in
+                // Trigger the twinkle animation when level up occurs
+                showTwinkle = true
+            }
             applyTheme(theme: themeVal.theme)
         }
         .onDisappear {
             NotificationCenter.default.removeObserver(self, name: progressManagerType.progressUpdatedNotification, object: nil)
+            NotificationCenter.default.removeObserver(self, name: UserDefaultsSeedProgressManager.levelUpNotification, object: nil)
         }
         .onChange(of: themeVal) { newThemeValue in
             applyTheme(theme: newThemeValue.theme)
