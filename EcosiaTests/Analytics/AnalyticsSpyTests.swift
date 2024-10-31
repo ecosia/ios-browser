1239ae42c3a4330453825ad9bd84583af56c1d20
Luca Schifino <luca.schifino@ecosia.org>
2024-10-31 11:43:43 +0100

    Add menu status tests

--- EcosiaTests/Analytics/AnalyticsSpyTests.swift
+++ EcosiaTests/Analytics/AnalyticsSpyTests.swift
@@ -26,6 +26,13 @@ final class AnalyticsSpy: Analytics {
     override func menuShare(_ content: Analytics.Property.ShareContent) {
         menuShareContentCalled = content
     }
+    
+    var menuStatusItemCalled: Analytics.Label.MenuStatus?
+    var menuStatusItemChangedTo: Bool?
+    override func menuStatus(changed item: Analytics.Label.MenuStatus, to: Bool) {
+        menuStatusItemCalled = item
+        menuStatusItemChangedTo = to
+    }
 }
 
 final class AnalyticsSpyTests: XCTestCase {
@@ -156,5 +163,46 @@ final class AnalyticsSpyTests: XCTestCase {
         }
     }
     
-    // TODO: Add menuStatus tests
+    func testTrackMenuStatus() {
+        let testCases: [(Analytics.Label.MenuStatus, Bool, String, ((String) -> Void)?)] = [
+            (.readingList, true, .ShareAddToReadingList, nil),
+            (.readingList, false, .AppMenu.RemoveReadingList, self.setReadingList),
+            (.bookmark, true, .KeyboardShortcuts.AddBookmark, nil),
+            // TODO: Investigate why `setBookmark` doesn't work
+            // (.bookmark, false, .RemoveBookmarkContextMenuTitle, self.setBookmark),
+            (.shortcut, true, .AddToShortcutsActionTitle, nil),
+            (.shortcut, false, .AppMenu.RemoveFromShortcuts, self.setBookmark)
+        ]
+        for (label, value, title, given) in testCases {
+            analyticsSpy = AnalyticsSpy()
+            Analytics.shared = analyticsSpy
+            XCTContext.runActivity(named: "Menu share \(label.rawValue) is tracked") { _ in
+                XCTAssertNil(analyticsSpy.menuStatusItemCalled)
+                XCTAssertNil(analyticsSpy.menuStatusItemChangedTo)
+                
+                let testUrl = "https://example.com"
+                given?(testUrl)
+                tabManagerMock.selectedTab?.url = URL(string: testUrl)
+                
+                let expectation = self.expectation(description: "Actions are returned")
+                menuHelper.getToolbarActions(navigationController: .init()) { actions in
+                    let action = actions
+                        .flatMap { $0 } // Flattens sections
+                        .flatMap { $0.items } // Flattens items in sections
+                        .first { $0.title == title }
+                    if let action = action {
+                        action.tapHandler!(action)
+                        
+                        XCTAssertEqual(self.analyticsSpy.menuStatusItemCalled, label)
+                        XCTAssertEqual(self.analyticsSpy.menuStatusItemChangedTo, value)
+                    } else {
+                        XCTFail("No action title with \(title) found")
+                    }
+                    
+                    expectation.fulfill()
+                }
+                wait(for: [expectation], timeout: 1)
+            }
+        }
+    }
 }
