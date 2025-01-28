8346c6deb672dffaac8beb798f5510b55e219b42
Dario Carlomagno <dario.carlomagno@ecosia.org>
2025-01-28 14:59:23 +0100

    [MOB-3113] Move into the correct Snapshot Tests folder

--- perform_snapshot_tests.sh
+++ perform_snapshot_tests.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:508b0ae40131a73babc893f136cb6473bb00a52acfd11a372355705378cba741
-size 16298
+oid sha256:801e39b211fcd053ca4df9294f1f2f2b5517f878ac319eb16faef3bd76ed7ce0
+size 16314
