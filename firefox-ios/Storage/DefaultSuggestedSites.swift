43a7d6844bc63a20eb37c1e4f8f459c86d5285c2
Dario Carlomagno <dario.carlomagno@ecosia.org>
2025-01-28 14:15:04 +0100

    [MOB-3113] Default Suggested Sites update

--- firefox-ios/Storage/DefaultSuggestedSites.swift
+++ firefox-ios/Storage/DefaultSuggestedSites.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:04d02a49ef425d8f1886bd1b6eca7f4b1a1d9ec2f0733705e63188b77a337d4c
-size 6617
+oid sha256:8c9faa70c6ddc0e28eae417b0115d23228ac66ecf81807555ea4461ecfa17631
+size 7978
