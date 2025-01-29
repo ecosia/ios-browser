6f2570612f46eff1741d0963fbb54fad873af508
Dario Carlomagno <dario.carlomagno@ecosia.org>
2025-01-29 17:21:00 +0100

    [MOB-3113] Add correct cached SPM folder

--- perform_snapshot_tests.sh
+++ perform_snapshot_tests.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:726b144a48ab6b9972e5387fd71078bfa0ce7edb8f09487a4207e46952e59df0
+oid sha256:cc1f6fdf34ccbe5c8e6029e72a1604b22e00b2c01147e4951f6729df263ad1aa
 size 16344
