374bb20dccba8be8606b1abbe6ac2c7e91b991d7
Dario Carlomagno <dario.carlomagno@ecosia.org>
2025-01-29 22:38:10 +0100

    [MOB-3113] Rename `testPlans` in `testBundle` as we rely on those eventually.

--- perform_snapshot_tests.sh
+++ perform_snapshot_tests.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:cc1f6fdf34ccbe5c8e6029e72a1604b22e00b2c01147e4951f6729df263ad1aa
-size 16344
+oid sha256:49465606ba621760934b55a387e71c88c55e8e4d38ca50de070cc28691f7799b
+size 16367
