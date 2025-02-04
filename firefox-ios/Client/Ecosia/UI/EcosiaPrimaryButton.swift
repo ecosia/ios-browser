64536e241abc1e85734f0a8d1777b7b9ab2fba7f
Dario Carlomagno <dario.carlomagno@ecosia.org>
2025-02-04 11:38:08 +0100

    [MOB-3113] Fix Primary button initialization

--- firefox-ios/Client/Ecosia/UI/EcosiaPrimaryButton.swift
+++ firefox-ios/Client/Ecosia/UI/EcosiaPrimaryButton.swift
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:770ccb284a20194a4dc008d68c9531083eb62e3dc797512e2d173497fec5aae8
+oid sha256:86ca2b30eea38176a1938fdb9014039fbd9170ff21d9ec8686fc8ca6e5537873
 size 1167
