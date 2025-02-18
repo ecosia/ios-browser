1880507c6ca8d75c1511ecbc414fa524ea6ff81f
Dario Carlomagno <dario.carlomagno@ecosia.org>
2025-02-18 11:30:07 +0100

    Auth0 brought into the upgrade

---
+++ firefox-ios/Client/Ecosia/UI/NTP/Account/NTPAccountLoginViewModel.swift
@@ -0,0 +1,75 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import Common
+import Foundation
+import Shared
+
+final class NTPAccountLoginViewModel {
+    struct UX {
+        static let topInset: CGFloat = 24
+    }
+
+    private let profile: Profile
+    weak var delegate: NTPSeedCounterDelegate?
+    var onTapAction: ((UIButton) -> Void)?
+    var theme: Theme
+
+    init(profile: Profile, theme: Theme) {
+        self.profile = profile
+        self.theme = theme
+    }
+}
+
+// MARK: HomeViewModelProtocol
+extension NTPAccountLoginViewModel: HomepageViewModelProtocol, FeatureFlaggable {
+    var sectionType: HomepageSectionType {
+        return .accountLogin
+    }
+
+    var headerViewModel: LabelButtonHeaderViewModel {
+        return .emptyHeader
+    }
+
+    func section(for traitCollection: UITraitCollection, size: CGSize) -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                              heightDimension: .estimated(30))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1),
+                                               heightDimension: .estimated(30))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 1)
+
+        let section = NSCollectionLayoutSection(group: group)
+
+        section.contentInsets = NSDirectionalEdgeInsets(
+            top: UX.topInset,
+            leading: 0,
+            bottom: 0,
+            trailing: 0)
+
+        return section
+    }
+
+    func numberOfItemsInSection() -> Int {
+        return 1
+    }
+
+    var isEnabled: Bool {
+        true
+    }
+
+    func setTheme(theme: Theme) {
+        self.theme = theme
+    }
+}
+
+extension NTPAccountLoginViewModel: HomepageSectionHandler {
+
+    func configure(_ cell: UICollectionViewCell, at indexPath: IndexPath) -> UICollectionViewCell {
+        guard let accountLoginCell = cell as? NTPAccountLoginCell else { return UICollectionViewCell() }
+        accountLoginCell.applyTheme(theme: theme)
+        return accountLoginCell
+    }
+}
