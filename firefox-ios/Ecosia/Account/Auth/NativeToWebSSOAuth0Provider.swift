d575b8646345ad1eb5210035bbf98c3061d2f40d
Dario Carlomagno <dario.carlomagno@ecosia.org>
2025-02-19 17:17:49 +0100

    [MOB-3226] Small refactor. Add structs for request and response

--- firefox-ios/Ecosia/Account/Auth/NativeToWebSSOAuth0Provider.swift
+++ firefox-ios/Ecosia/Account/Auth/NativeToWebSSOAuth0Provider.swift
@@ -9,14 +9,13 @@ public struct NativeToWebSSOAuth0Provider: Auth0ProviderProtocol {
 
     private let credentialsManager = CredentialsManager(authentication: Auth0.authentication(bundle: .ecosia),
                                                         storage: EcosiaKeychainStorage())
-    
-    typealias SessionToken = String
-    
+
+    public typealias SessionToken = String
+
     enum NativeToWebSSOError: Error {
         case invalidResponse
         case missingRefreshToken(String)
         case missingConfiguration(String)
-        case couldNotRetrieveToken
     }
 
     public init() {}
@@ -78,14 +77,65 @@ public struct NativeToWebSSOAuth0Provider: Auth0ProviderProtocol {
 }
 
 extension NativeToWebSSOAuth0Provider {
-    
-    /// Requests the `session_token` with the `refresh_token`
+
+    /// Struct for encoding the JSON request body
+    struct TokenRequest: Codable {
+        let grantType: String
+        let subjectToken: String
+        let subjectTokenType: String
+        let requestedTokenType: String
+        let clientId: String
+    }
+
+    /// Struct for decoding the JSON response
+    struct TokenResponse: Codable {
+        let accessToken: String
+    }
+
+    /// Requests the `session_token` with the `refresh_token`.
     ///
-    /// - Returns: A `session_token` as `SessionToken` as a `String` type
+    /// - Returns: A `session_token` as `SessionToken` (a `String` type).
     /// - Throws: An error if the retrieval fails.
-    public func getSessionToken() async throws -> String {
-        
-        let credentials = try await withCheckedThrowingContinuation { continuation in
+    public func getSessionToken() async throws -> SessionToken {
+        let credentials = try await retrieveStoredCredentials()
+        guard let refreshToken = credentials.refreshToken else {
+            throw NativeToWebSSOError.missingRefreshToken("Refresh token is missing. Please check your credentials.")
+        }
+
+        guard let values = configurationValues() else {
+            throw NativeToWebSSOError.missingConfiguration("Missing Auth0.plist file")
+        }
+
+        let tokenExchangeUrl = URL(string: "https://\(values.domain)/oauth/token")!
+        let tokenRequest = TokenRequest(
+            grantType: "urn:ietf:params:oauth:grant-type:token-exchange",
+            subjectToken: refreshToken,
+            subjectTokenType: "urn:ietf:params:oauth:token-type:refresh_token",
+            requestedTokenType: "urn:auth0:params:oauth:token-type:session_token",
+            clientId: values.clientId
+        )
+
+        var request = URLRequest(url: tokenExchangeUrl)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = try JSONEncoder().encode(tokenRequest)
+
+        let (data, response) = try await URLSession.shared.data(for: request)
+
+        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
+            throw NativeToWebSSOError.invalidResponse
+        }
+
+        let tokenResponse = try JSONDecoder().decode(TokenResponse.self, from: data)
+        return tokenResponse.accessToken
+    }
+
+    /// Retrieves stored credentials asynchronously.
+    ///
+    /// - Returns: The stored `Credentials` object if available.
+    /// - Throws: An error if retrieving credentials fails.
+    private func retrieveStoredCredentials() async throws -> Credentials {
+        return try await withCheckedThrowingContinuation { continuation in
             credentialsManager.credentials { result in
                 switch result {
                 case .success(let credentials):
@@ -95,57 +145,24 @@ extension NativeToWebSSOAuth0Provider {
                 }
             }
         }
-        
-        guard let refreshToken = credentials.refreshToken else {
-            throw NativeToWebSSOError.missingRefreshToken("Refresh token is missing. Please check your credentials.")
-        }
-        
-        guard let values = configurationValues() else {
-            throw NativeToWebSSOError.missingConfiguration("Missing Auth0.plist file")
-        }
-        
-        let tokenExchanceUrl = URL(string: "https://\(values.domain)/oauth/token")!
-        
-        let jsonBody: [String: Any] = [
-            "grant_type": "urn:ietf:params:oauth:grant-type:token-exchange",
-            "subject_token": refreshToken,
-            "subject_token_type": "urn:ietf:params:oauth:token-type:refresh_token",
-            "requested_token_type": "urn:auth0:params:oauth:token-type:session_token",
-            "client_id": values.clientId
-        ]
-        
-        var request = URLRequest(url: tokenExchanceUrl)
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.httpBody = try? JSONSerialization.data(withJSONObject: jsonBody, options: [])
-        
-        
-        let (data, response) = try await URLSession.shared.data(for: request)
-        
-        guard let httpResponse = response as? HTTPURLResponse, httpResponse.statusCode == 200 else {
-            throw NativeToWebSSOError.invalidResponse
-        }
-        
-        if let jsonResponse = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-           let sessionToken = jsonResponse["access_token"] as? String {
-            return sessionToken
-        } else {
-            throw NativeToWebSSOError.couldNotRetrieveToken
-        }
     }
-    
+
+    /// Retrieves configuration values from the Auth0.plist file.
+    ///
+    /// - Parameter bundle: The bundle containing the Auth0.plist file. Defaults to `.ecosia`.
+    /// - Returns: A tuple containing the `clientId` and `domain` if available, otherwise `nil`.
     func configurationValues(bundle: Bundle = .ecosia) -> (clientId: String, domain: String)? {
         guard let path = bundle.path(forResource: "Auth0", ofType: "plist"),
               let values = NSDictionary(contentsOfFile: path) as? [String: Any] else {
-                print("Missing Auth0.plist file with 'ClientId' and 'Domain' entries in main bundle!")
-                return nil
-            }
+            print("Missing Auth0.plist file with 'ClientId' and 'Domain' entries in main bundle!")
+            return nil
+        }
 
         guard let clientId = values["ClientId"] as? String, let domain = values["Domain"] as? String else {
-                print("Auth0.plist file at \(path) is missing 'ClientId' and/or 'Domain' entries!")
-                print("File currently has the following entries: \(values)")
-                return nil
-            }
+            print("Auth0.plist file at \(path) is missing 'ClientId' and/or 'Domain' entries!")
+            print("File currently has the following entries: \(values)")
+            return nil
+        }
         return (clientId: clientId, domain: domain)
     }
 }
