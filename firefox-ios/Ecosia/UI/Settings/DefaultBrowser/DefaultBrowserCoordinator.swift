6799766c29f281bff009eb6e0f161c1fe8e59f70
Dario Carlomagno <dario.carlomagno@ecosia.org>
2025-04-09 15:55:50 +0200

    [MOB-3199] Simplify DefaultBrowserCoordinator's detail show

--- firefox-ios/Ecosia/UI/Settings/DefaultBrowser/DefaultBrowserCoordinator.swift
+++ firefox-ios/Ecosia/UI/Settings/DefaultBrowser/DefaultBrowserCoordinator.swift
@@ -4,6 +4,7 @@
 
 import SwiftUI
 import Lottie
+import Common
 
 public struct DefaultBrowserCoordinator {
     let navigationController: UINavigationController
@@ -64,6 +65,35 @@ public struct DefaultBrowserCoordinator {
             action: #selector(DetailViewDoneHandler.handleDone)
         )
         navigationController.pushViewController(hostingController, animated: true)
+    }
+}
+
+extension DefaultBrowserCoordinator {
+    
+    public static func makeDefaultCoordinatorAndShowDetailViewFrom(_ navigationController: UINavigationController?,
+                                                            topViewContentBackground: Color,
+                                                            with theme: Theme) {
+        
+        guard let navigationController = navigationController else { return }
+        
+        let style = InstructionStepsViewStyle(
+            backgroundPrimaryColor: Color(theme.colors.ecosia.backgroundSecondary),
+            stepsBackgroundColor: Color(theme.colors.ecosia.backgroundPrimary),
+            textPrimaryColor: Color(theme.colors.ecosia.textPrimary),
+            textSecondaryColor: Color(theme.colors.ecosia.textSecondary),
+            buttonBackgroundColor: Color(theme.colors.ecosia.buttonBackgroundPrimaryActive),
+            buttonTextColor: Color(theme.colors.ecosia.textInversePrimary),
+            stepRowStyle: StepRowStyle(
+                stepNumberColor: Color(theme.colors.ecosia.textPrimary),
+                stepNumberBackgroundColor: Color(theme.colors.ecosia.backgroundSecondary),
+                stepTextColor: Color(theme.colors.ecosia.textPrimary)
+            )
+        )
+
+        let coordinator = DefaultBrowserCoordinator(navigationController: navigationController,
+                                                    style: style,
+                                                    customTopContentViewBackground: topViewContentBackground)
+        coordinator.showDetailView()
     }
 }
 
