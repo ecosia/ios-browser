f3bb015c3448b3fe8cf59aa65da5bfbebfeb2cfc
Dario Carlomagno <dario.carlomagno@ecosia.org>
2025-04-09 15:36:01 +0200

    [MOB-3199] Further optimization to minimize code lines

--- firefox-ios/Client/Ecosia/Extensions/AppSettingsTableViewController+Ecosia.swift
+++ firefox-ios/Client/Ecosia/Extensions/AppSettingsTableViewController+Ecosia.swift
@@ -187,5 +187,36 @@ extension AppSettingsTableViewController {
         isDefautlBrowserCell(section) &&
         User.shared.shouldShowDefaultBrowserSettingNudgeCard
     }
+    
+    func hideDefaultBrowserNudgeCardInSection(_ section: Int) {
+        guard section < settings.count else { return }
+        self.settings.remove(at: section)
+        self.tableView.deleteSections(IndexSet(integer: section), with: .automatic)
+    }
+
+    func showDefaultBrowserDetailView() {
+        guard let navigationController = self.navigationController else { return }
+
+        let theme = themeManager.getCurrentTheme(for: windowUUID)
+        let style = InstructionStepsViewStyle(
+            backgroundPrimaryColor: theme.colors.ecosia.backgroundSecondary.color,
+            stepsBackgroundColor: theme.colors.ecosia.backgroundPrimary.color,
+            textPrimaryColor: theme.colors.ecosia.textPrimary.color,
+            textSecondaryColor: theme.colors.ecosia.textSecondary.color,
+            buttonBackgroundColor: theme.colors.ecosia.buttonBackgroundPrimaryActive.color,
+            buttonTextColor: theme.colors.ecosia.textInversePrimary.color,
+            stepRowStyle: StepRowStyle(
+                stepNumberColor: theme.colors.ecosia.textPrimary.color,
+                stepNumberBackgroundColor: theme.colors.ecosia.backgroundSecondary.color,
+                stepTextColor: theme.colors.ecosia.textPrimary.color
+            )
+        )
+
+        let coordinator = DefaultBrowserCoordinator(navigationController: navigationController,
+                                                    style: style,
+                                                    customTopContentViewBackground:
+                                                        EcosiaColor.DarkGreen50.color)
+        coordinator.showDetailView()
+    }
 }
 
