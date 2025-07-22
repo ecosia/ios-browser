f19d41087f0f831c4bcb48be2e52fde3a9402bec
Dario Carlomagno <dario.carlomagno@ecosia.org>
2025-07-22 16:06:40 +0200

    [MOB-3603] chore: swiftlint fix

--- firefox-ios/Ecosia/Core/Environment/Environment.swift
+++ firefox-ios/Ecosia/Core/Environment/Environment.swift
@@ -34,13 +34,13 @@ extension Environment {
             return .debug
         }
     }
-    
+
     // Alternative: Bundle ID based detection
     public static var currentFromBundleId: Environment {
         guard let bundleId = Bundle.main.bundleIdentifier else {
             return .debug
         }
-        
+
         switch bundleId {
         case "com.ecosia.ecosiaapp":
             return .production
@@ -73,7 +73,7 @@ extension Environment {
               let envString = plist["EcosiaEnvironment"] as? String else {
             return .debug
         }
-        
+
         switch envString.lowercased() {
         case "production":
             return .production
