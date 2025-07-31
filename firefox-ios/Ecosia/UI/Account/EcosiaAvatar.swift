17ef29f9eddd39bce4bdfdbc37a670f04bb98357
Dario Carlomagno <dario.carlomagno@ecosia.org>
2025-07-31 14:28:01 +0200

    [MOB-3543] Complete NTP account login implementation with user cancellation fix
    - Implement NTPAccountLoginCell with SwiftUI integration
    - Add reusable UI components: EcosiaAvatar, EcosiaSeedView, EcosiaAccountNavButton
    - Integrate Auth0 user profile fetching and avatar display
    - Fix user cancellation handling in authentication flows
    - Add AuthError.userCancelled case for proper error mapping
    - Apply Ecosia Design System tokens throughout components

---
+++ firefox-ios/Ecosia/UI/Account/EcosiaAvatar.swift
@@ -0,0 +1,78 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+import SwiftUI
+
+/// A reusable avatar component that displays user avatars with remote image loading
+public struct EcosiaAvatar: View {
+    private let avatarURL: URL?
+    private let size: CGFloat
+    private let placeholderImage: String
+
+    public init(
+        avatarURL: URL?,
+        size: CGFloat = .ecosia.space._2l,
+        placeholderImage: String = "avatar"
+    ) {
+        self.avatarURL = avatarURL
+        self.size = size
+        self.placeholderImage = placeholderImage
+    }
+
+    public var body: some View {
+        Group {
+            if let avatarURL = avatarURL {
+                AsyncImage(url: avatarURL) { image in
+                    image
+                        .resizable()
+                        .scaledToFill()
+                } placeholder: {
+                    placeholderView
+                }
+                .frame(width: size, height: size)
+                .clipShape(Circle())
+            } else {
+                placeholderView
+            }
+        }
+    }
+
+    private var placeholderView: some View {
+        Image(placeholderImage, bundle: .ecosia)
+            .resizable()
+            .scaledToFit()
+            .frame(width: size, height: size)
+            .clipShape(Circle())
+    }
+}
+
+#if DEBUG
+// MARK: - Preview
+@available(iOS 16.0, *)
+struct EcosiaAvatar_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack(spacing: .ecosia.space._l) {
+            // With remote URL
+            EcosiaAvatar(
+                avatarURL: URL(string: "https://avatars.githubusercontent.com/u/1?v=4"),
+                size: .ecosia.space._6l
+            )
+
+            // Without URL (placeholder)
+            EcosiaAvatar(
+                avatarURL: nil,
+                size: .ecosia.space._6l
+            )
+
+            // Small size
+            EcosiaAvatar(
+                avatarURL: URL(string: "https://avatars.githubusercontent.com/u/1?v=4"),
+                size: .ecosia.space._2l
+            )
+        }
+        .padding()
+        .previewLayout(.sizeThatFits)
+    }
+}
+#endif
