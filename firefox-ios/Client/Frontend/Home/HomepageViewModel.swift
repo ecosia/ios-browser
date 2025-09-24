a48a882c9335bbfef6c0f0e59f3be5f327c0d311
Dario Carlomagno <dario.carlomagno@ecosia.org>
2025-09-24 21:54:41 +0200

    [MOB-3635] Remove climateImpactCounterViewModel from homepage components
    - Remove climateImpactCounterViewModel property and initialization from HomepageViewModel
    - Remove climate impact counter delegate assignments and notification handling from LegacyHomepageViewController
    - Clean up seed counter experiment tracking and level up logic
    - Maintain homepage functionality without climate impact counter dependencies

--- firefox-ios/Client/Frontend/Home/HomepageViewModel.swift
+++ firefox-ios/Client/Frontend/Home/HomepageViewModel.swift
@@ -137,7 +137,6 @@ class HomepageViewModel: FeatureFlaggable, InjectedThemeUUIDIdentifiable {
     var impactViewModel: NTPImpactCellViewModel
     var newsViewModel: NTPNewsCellViewModel
     var ntpCustomizationViewModel: NTPCustomizationCellViewModel
-    var climateImpactCounterViewModel: NTPSeedCounterViewModel
 
     /*
      Ecosia: Represents the container that stores some of the `HomepageSectionType`s.
@@ -184,7 +183,6 @@ class HomepageViewModel: FeatureFlaggable, InjectedThemeUUIDIdentifiable {
         self.impactViewModel = NTPImpactCellViewModel(referrals: referrals, theme: theme)
         self.newsViewModel = NTPNewsCellViewModel(theme: theme)
         self.ntpCustomizationViewModel = NTPCustomizationCellViewModel(theme: theme)
-        self.climateImpactCounterViewModel = NTPSeedCounterViewModel(profile: profile, theme: theme)
         self.wallpaperManager = wallpaperManager
         /* Ecosia: Remove `jumpBackIn` section reference
         let jumpBackInAdaptor = JumpBackInDataAdaptorImplementation(profile: profile,
@@ -239,7 +237,6 @@ class HomepageViewModel: FeatureFlaggable, InjectedThemeUUIDIdentifiable {
          */
         // Ecosia: Those models needs to follow strictly the order defined in `enum HomepageSectionType`
         self.childViewModels = [multiPurposeEcosiaHeaderViewModel,
-                                climateImpactCounterViewModel,
                                 headerViewModel,
                                 libraryViewModel,
                                 topSiteViewModel,
