58d341242586c9a40d4046da38c579c1a026cc7a
Dario Carlomagno <dario.carlomagno@ecosia.org>
2025-10-24 18:40:35 +0200

    [MOB-3924] Disable Accounts on NTP via unleash flag

--- firefox-ios/Client/Ecosia/UI/NTP/Header/NTPHeader.swift
+++ firefox-ios/Client/Ecosia/UI/NTP/Header/NTPHeader.swift
@@ -85,21 +85,23 @@ struct NTPHeaderView: View {
                     onTap: handleAISearchTap
                 )
             }
-            ZStack(alignment: .topLeading) {
-                EcosiaAccountNavButton(
-                    seedCount: viewModel.seedCount,
-                    avatarURL: viewModel.userAvatarURL,
-                    enableAnimation: !reduceMotion,
-                    windowUUID: windowUUID,
-                    onTap: handleTap
-                )
-
-                if let increment = viewModel.balanceIncrement {
-                    BalanceIncrementAnimationView(
-                        increment: increment,
-                        windowUUID: windowUUID
+            if AccountFeatureExperiment.isEnabled {
+                ZStack(alignment: .topLeading) {
+                    EcosiaAccountNavButton(
+                        seedCount: viewModel.seedCount,
+                        avatarURL: viewModel.userAvatarURL,
+                        enableAnimation: !reduceMotion,
+                        windowUUID: windowUUID,
+                        onTap: handleTap
                     )
-                    .offset(x: 20, y: -10)
+
+                    if let increment = viewModel.balanceIncrement {
+                        BalanceIncrementAnimationView(
+                            increment: increment,
+                            windowUUID: windowUUID
+                        )
+                        .offset(x: 20, y: -10)
+                    }
                 }
             }
         }
