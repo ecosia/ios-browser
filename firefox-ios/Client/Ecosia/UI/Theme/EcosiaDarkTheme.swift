5c13b982cf68621bcb0e5b802cb53a55936d19c4
Dario Carlomagno <dario.carlomagno@ecosia.org>
2025-11-03 18:25:44 +0100

    [MOB-3538] Fixes after rebase with `main`

--- firefox-ios/Client/Ecosia/UI/Theme/EcosiaDarkTheme.swift
+++ firefox-ios/Client/Ecosia/UI/Theme/EcosiaDarkTheme.swift
@@ -26,13 +26,16 @@ private struct EcosiaDarkSemanticColors: EcosiaSemanticColors {
     var backgroundFeatured: UIColor = EcosiaColor.Grellow100
     var backgroundPrimary: UIColor = EcosiaColor.Gray90
     var backgroundPrimaryDecorative: UIColor = EcosiaColor.Gray90
+    var backgroundRoleNegative: UIColor = EcosiaColor.Claret800
     var backgroundSecondary: UIColor = EcosiaColor.Gray80
     var backgroundTertiary: UIColor = EcosiaColor.Gray70
     var backgroundQuaternary: UIColor = EcosiaColor.Gray70
     var backgroundElevation1: UIColor = EcosiaColor.Gray80
     var backgroundElevation2: UIColor = EcosiaColor.Gray70
     var borderDecorative: UIColor = EcosiaColor.Gray60
+    var borderNegative: UIColor = EcosiaColor.Claret600
     var brandFeatured: UIColor = EcosiaColor.Grellow100
+    var brandImpact: UIColor = EcosiaColor.Yellow40
     var brandPrimary: UIColor = EcosiaColor.White
     var buttonBackgroundFeatured: UIColor = EcosiaColor.Grellow100
     var buttonBackgroundFeaturedActive: UIColor = EcosiaColor.Grellow300
@@ -45,6 +48,7 @@ private struct EcosiaDarkSemanticColors: EcosiaSemanticColors {
     var buttonContentSecondary: UIColor = EcosiaColor.White
     var buttonContentSecondaryStatic: UIColor = EcosiaColor.Gray70
     var highlighter: UIColor = EcosiaColor.Grellow100.withAlphaComponent(0.32)
+    var linkPrimary: UIColor = EcosiaColor.White
     var iconDecorative: UIColor = EcosiaColor.White
     var iconInverseStrong: UIColor = EcosiaColor.Black
     var segmentedControlBackgroundActive: UIColor = EcosiaColor.Gray60
