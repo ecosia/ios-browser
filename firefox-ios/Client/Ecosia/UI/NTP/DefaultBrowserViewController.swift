cfc3bd552c022f11d774be3408317ab556676077
Tim Nowaczynski <TimNowaczynski@quarian.de>
2026-02-17 17:42:19 +0100

    [MOB-3548] MOB-3548: reverted accidentially commited line

--- firefox-ios/Client/Ecosia/UI/NTP/DefaultBrowserViewController.swift
+++ firefox-ios/Client/Ecosia/UI/NTP/DefaultBrowserViewController.swift
@@ -29,6 +29,7 @@ final class DefaultBrowserViewController: UIViewController, Themeable {
     }()
     private lazy var imageView: UIImageView = {
         let view = UIImageView(image: .init(named: "defaultBrowser"))
+        view.contentMode = .scaleAspectFill
         view.clipsToBounds = true
         view.translatesAutoresizingMaskIntoConstraints = false
         view.setContentCompressionResistancePriority(.required, for: .vertical)
