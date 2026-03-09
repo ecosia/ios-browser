cbfff34fbcc0b3a660dbb083aa8bed749f637a81
Luca Schifino <luca.schifino@ecosia.org>
2026-03-09 12:34:46 +0100

    [MOB-3544] Add onboarding product tour ab test context

--- firefox-ios/Ecosia/Analytics/Analytics.swift
+++ firefox-ios/Ecosia/Analytics/Analytics.swift
@@ -483,7 +483,7 @@ extension Analytics {
     func appendActivityContextIfNeeded(_ action: Analytics.Action.Activity, _ event: Structured, completion: @escaping () -> Void) {
         switch action {
         case .resume, .launch:
-            addABTestContexts(to: event, toggles: [.brazeIntegration])
+            addABTestContexts(to: event, toggles: [.brazeIntegration, .onboardingProductTour])
             addCookieConsentContext(to: event)
             addUserStateContext(to: event, completion: completion)
         }
