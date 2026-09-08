0564c2931e2c5a0f4ea8bdde2c79f36246c2478b
Paul <paul@paulquinney.com>
2026-09-08 23:47:43 +0200

    corrects staging auth0 id

--- tuist-setup.sh
+++ tuist-setup.sh
@@ -126,7 +126,7 @@ if [ ! -f "$file_path" ]; then
     touch "$file_path"
     echo -e "${GREEN}✓ Staging.xcconfig created${NC}\n"
     {
-        echo "AUTH0_CLIENT_ID=Msl19FhlbwPk6IGQw2D8gzmyTwyeRG5D"
+        echo "AUTH0_CLIENT_ID=zNU6cgqji5cE9qPkkXIlqMJbIwTPShdU"
         echo "CF_ACCESS_CLIENT_ID=$CF_ACCESS_CLIENT_ID"
         echo "CF_ACCESS_CLIENT_SECRET=$CF_ACCESS_CLIENT_SECRET"
     } >> $file_path
